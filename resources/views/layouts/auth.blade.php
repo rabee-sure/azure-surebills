ca7e2686620f115188dd8fb62ea896841f169fb2
abdullah ghanem <aghanem@sure.com.sa>
2020-06-18 10:52:56 +0200

    js validation

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,10 +5,7 @@
     <title>{{ config('app.name', 'Sure Bills') }} - @yield('title')</title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<!-- Scripts -->
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
-     <script type="text/javascript" src="{{ asset('vendor/jsvalidation/js/jsvalidation.js')}}"></script>
+
     <!-- Scripts -->
     <script src="{{ asset('js/auth.js') }}" defer></script>
 
@@ -22,7 +19,15 @@
         @yield('content')
       </div>
     </main>
+<!-- Scripts -->
+ <!-- Scripts -->
+ <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+ <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
 
+ <!-- Laravel Javascript Validation -->
+ <script type="text/javascript" src="{{ asset('vendor/jsvalidation/js/jsvalidation.js')}}"></script>
       @yield('footer-scripts')
+
+      
   </body>
 </html>
