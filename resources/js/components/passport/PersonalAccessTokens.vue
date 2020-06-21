ded1ea44ec55820282ecbcf8aec6b4e5df951d96
Ahmed-Rabee <dev.ahmedrabee@gmail.com>
2020-06-21 13:30:10 +0200

    add style to integration files

--- resources/js/components/passport/PersonalAccessTokens.vue
+++ resources/js/components/passport/PersonalAccessTokens.vue
@@ -1,157 +1,109 @@
-<style scoped>
-    .action-link {
-        cursor: pointer;
-    }
-</style>
-
 <template>
-    <div>
-        <div>
-            <div class="card card-default">
-                <div class="card-header">
-                    <div style="display: flex; justify-content: space-between; align-items: center;">
-                        <span>
-                            Personal Access Tokens
-                        </span>
-
-                        <a class="action-link" tabindex="-1" @click="showCreateTokenForm">
-                            Create New Token
-                        </a>
-                    </div>
-                </div>
-
-                <div class="card-body">
-                    <!-- No Tokens Notice -->
-                    <p class="mb-0" v-if="tokens.length === 0">
-                        You have not created any personal access tokens.
-                    </p>
-
-                    <!-- Personal Access Tokens -->
-                    <table class="table table-borderless mb-0" v-if="tokens.length > 0">
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            <tr v-for="token in tokens">
-                                <!-- Client Name -->
-                                <td style="vertical-align: middle;">
-                                    {{ token.name }}
-                                </td>
-
-                                <!-- Delete Button -->
-                                <td style="vertical-align: middle;">
-                                    <a class="action-link text-danger" @click="revoke(token)">
-                                        Delete
-                                    </a>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+  <div class="Personal_Access_Tokenscard">
+    <div class="card card-default">
+      <div class="card-header">
+        <span>Personal Access Tokens</span>
+        <a class="btn btn-primary" tabindex="-1" @click="showCreateTokenForm">Create New Token</a>
+      </div>
+      <div class="card-body">
+        <!-- No Tokens Notice -->
+        <div class="not_created_personal" v-if="tokens.length === 0">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 333331 267872" shape-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" xmlns:v="https://vecta.io/nano"><path d="M208099 64019l42760 43315v20375l-20232 1v-15317h-16762V95629h-16043l-4638-15109c-7383 4925-16344 7811-25999 7811-25290 0-45802-19778-45802-44164C121383 19777 141895 0 167185 0c25287 0 45797 19778 45797 44167 0 7140-1761 13883-4881 19852h-1zM0 123339h67357v119843H0V123339zm80916 109783V132962h45050c19099 3425 38197 13780 57297 25805h34991c15839 955 24133 17007 8748 27555-12275 8996-28460 8487-45050 6998-11442-563-11943 14819 0 14870 4140 321 8653-650 12588-654 20711-20 37770-3979 48209-20340l5248-12246 52049-25806c26047-8576 44555 18671 25368 37615-37699 27429-76366 50000-115906 68231-28721 17470-57443 16877-86164 0l-42426-21869zm79685-203106c4513 0 8177 3661 8177 8177s-3663 8177-8177 8177c-4516 0-8177-3661-8177-8177s3661-8177 8177-8177z"/></svg>
+          <span>You have not created any personal access tokens.</span>
         </div>
-
-        <!-- Create Token Modal -->
-        <div class="modal fade" id="modal-create-token" tabindex="-1" role="dialog">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h4 class="modal-title">
-                            Create Token
-                        </h4>
-
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    </div>
-
-                    <div class="modal-body">
-                        <!-- Form Errors -->
-                        <div class="alert alert-danger" v-if="form.errors.length > 0">
-                            <p class="mb-0"><strong>Whoops!</strong> Something went wrong!</p>
-                            <br>
-                            <ul>
-                                <li v-for="error in form.errors">
-                                    {{ error }}
-                                </li>
-                            </ul>
-                        </div>
-
-                        <!-- Create Token Form -->
-                        <form role="form" @submit.prevent="store">
-                            <!-- Name -->
-                            <div class="form-group row">
-                                <label class="col-md-4 col-form-label">Name</label>
-
-                                <div class="col-md-6">
-                                    <input id="create-token-name" type="text" class="form-control" name="name" v-model="form.name">
-                                </div>
-                            </div>
-
-                            <!-- Scopes -->
-                            <div class="form-group row" v-if="scopes.length > 0">
-                                <label class="col-md-4 col-form-label">Scopes</label>
-
-                                <div class="col-md-6">
-                                    <div v-for="scope in scopes">
-                                        <div class="checkbox">
-                                            <label>
-                                                <input type="checkbox"
-                                                    @click="toggleScope(scope.id)"
-                                                    :checked="scopeIsAssigned(scope.id)">
-
-                                                    {{ scope.id }}
-                                            </label>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-
-                    <!-- Modal Actions -->
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-
-                        <button type="button" class="btn btn-primary" @click="store">
-                            Create
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Access Token Modal -->
-        <div class="modal fade" id="modal-access-token" tabindex="-1" role="dialog">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h4 class="modal-title">
-                            Personal Access Token
-                        </h4>
-
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    </div>
-
-                    <div class="modal-body">
-                        <p>
-                            Here is your new personal access token. This is the only time it will be shown so don't lose it!
-                            You may now use this token to make API requests.
-                        </p>
-
-                        <textarea class="form-control" rows="10">{{ accessToken }}</textarea>
-                    </div>
-
-                    <!-- Modal Actions -->
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <!-- Personal Access Tokens -->
+        <table class="table table-borderless mb-0" v-if="tokens.length > 0">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="token in tokens">
+              <!-- Client Name -->
+              <td style="vertical-align: middle;">{{ token.name }}</td>
+              <!-- Delete Button -->
+              <td style="vertical-align: middle;">
+                <a class="action-link text-danger" @click="revoke(token)">Delete</a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
+
+    <!-- Create Token Modal -->
+    <div class="modal fade" id="modal-create-token" tabindex="-1" role="dialog">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title">Create Token</h4>
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+          </div>
+          <div class="modal-body">
+            <!-- Form Errors -->
+            <div class="alert alert-danger" v-if="form.errors.length > 0">
+              <p class="mb-0"><strong>Whoops!</strong> Something went wrong!</p>
+              <br>
+              <ul>
+                <li v-for="error in form.errors">{{ error }}</li>
+              </ul>
+            </div>
+            <!-- Create Token Form -->
+            <form role="form" @submit.prevent="store">
+              <!-- Name -->
+              <div class="form-group row">
+                <label class="col-md-4 col-form-label">Name</label>
+                <div class="col-md-6">
+                  <input id="create-token-name" type="text" class="form-control" name="name" v-model="form.name">
+                </div>
+              </div>
+              <!-- Scopes -->
+              <div class="form-group row" v-if="scopes.length > 0">
+                <label class="col-md-4 col-form-label">Scopes</label>
+                <div class="col-md-6">
+                  <div v-for="scope in scopes">
+                    <div class="checkbox">
+                      <label>
+                        <input type="checkbox" @click="toggleScope(scope.id)" :checked="scopeIsAssigned(scope.id)">
+                        {{ scope.id }}
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+          <!-- Modal Actions -->
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary" @click="store">Create</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Access Token Modal -->
+    <div class="modal fade" id="modal-access-token" tabindex="-1" role="dialog">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title">Personal Access Token</h4>
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+          </div>
+          <div class="modal-body">
+            <p>Here is your new personal access token. This is the only time it will be shown so don't lose it! <br> You may now use this token to make API requests.</p>
+            <textarea class="form-control" rows="10">{{ accessToken }}</textarea>
+          </div>
+          <!-- Modal Actions -->
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -296,3 +248,64 @@
         }
     }
 </script>
+
+
+<style lang="scss" scoped>
+.Personal_Access_Tokenscard {
+  margin: 20px auto;
+  .card-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    padding: 20px 20px 0px 20px;
+    span {
+      display: block;
+      font-weight: bold;
+      font-size: 17px;
+      text-transform: capitalize;
+      color: #000;
+      [class="body-dark-mode"] & {
+        color: #fff;
+      } /* Dark Mode */
+    } /* span */
+    a {
+      display: block;
+      cursor: pointer;
+      font-size: 14px;
+      border-radius: 4px;
+      padding: 5px 15px;
+      color: #fff;
+    } /* a */
+  } /* card-header */
+  .card-body {
+    padding: 20px;
+    .not_created_personal {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-flow: column;
+      flex-wrap: wrap;
+      padding: 30px 0;
+      min-height: 200px;
+      svg {
+        height: 110px;
+        fill: #ddd;
+        [class="body-dark-mode"] & {
+          fill: #666;
+        } /* Dark Mode */
+      } /* svg */
+      span {
+        display: block;
+        font-size: 16px;
+        margin: 20px auto 0;
+        text-transform: capitalize;
+        color: #777;
+        [class="body-dark-mode"] & {
+          color: #666;
+        } /* Dark Mode */
+      } /* span */
+    } /* not_created_personal */
+  } /* card-body */
+} /* Personal_Access_Tokenscard */
+</style>
--- resources/views/integration/index.blade.php
+++ resources/views/integration/index.blade.php
@@ -1,29 +1,25 @@
- @extends('layouts.app')
-
+@extends('layouts.app')
 @section('title', 'Page Title')
-
 @section('content')
-  <div class="row">
-    <div class="col-12">
-      <div class="mb-2">
-        <h1>{{__('Integration')}}</h1>
-        <nav class="breadcrumb-container d-none d-sm-block d-lg-inline-block" aria-label="breadcrumb">
-          <ol class="breadcrumb pt-0">
-            <li class="breadcrumb-item">
-              <a href="{{ route('home') }}">{{__('Home')}}</a>
-            </li>
-            <li class="breadcrumb-item active" aria-current="page">{{__('Integration')}}</li>
-          </ol>
-        </nav>
-      </div>
-
-      <div class="separator mb-5"></div>
+<div class="row">
+  <div class="col-12">
+    <div class="mb-2">
+      <h1>{{__('Integration')}}</h1>
+      <nav class="breadcrumb-container d-none d-sm-block d-lg-inline-block" aria-label="breadcrumb">
+        <ol class="breadcrumb pt-0">
+          <li class="breadcrumb-item">
+            <a href="{{ route('home') }}">{{__('Home')}}</a>
+          </li>
+          <li class="breadcrumb-item active" aria-current="page">{{__('Integration')}}</li>
+        </ol>
+      </nav>
     </div>
+    <div class="separator mb-5"></div>
   </div>
-
-  <div class="row">
-    <passport-clients></passport-clients>
-    <passport-authorized-clients></passport-authorized-clients>
-    <passport-personal-access-tokens></passport-personal-access-tokens>
-  </div>
+</div>
+<div class="integration_index_page">
+  <passport-clients></passport-clients>
+  <passport-authorized-clients></passport-authorized-clients>
+  <passport-personal-access-tokens></passport-personal-access-tokens>
+</div><!-- integration_index_page -->
 @endsection
