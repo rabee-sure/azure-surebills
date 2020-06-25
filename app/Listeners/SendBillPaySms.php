2e06074088293f69739f599c8048cc28a6353b72
Essam Abdel-Sabour <me@essam.ws>
2020-06-25 15:27:28 +0200

    fix send sms country code

--- app/Listeners/SendBillPaySms.php
+++ app/Listeners/SendBillPaySms.php
@@ -30,7 +30,7 @@ class SendBillPaySms
 
         if($event->bill->send_sms){
             if(app()->environment('production')){
-                $message = __('to pay bill please open link below: ',[],'en') . $event->bill->pay_url;
+                $message = __('هلاً :name<br>لديك فاتورة بقيمة :total SAR من :business_name يمكنك الدفع بكل سهولة الأن<br> :url', ['total' => $event->bill->total, 'business_name' => $event->bill->business_name, 'name' => $event->bill->customer_name, 'url' => $event->bill->pay_url]);
                 $message .= PHP_EOL;
                 $mobile = (int) $event->bill->customer_mobile;
                 $mobile = (int) '966'.$mobile;
