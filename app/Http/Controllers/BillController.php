4f081483b1f89b98a7567292bd804828c1c18bbe
Essam Abdel-Sabour <me@essam.ws>
2020-07-05 14:23:28 +0200

    bill 404 issue

--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -141,9 +141,12 @@ class BillController extends Controller
     {
         $bill = Bill::decodeId($id);
 
-        if($bill == null || $bill->is_invalid){
+        if(!$bill){
+            abort(404);
+        }
+
+        if($bill->is_invalid){
             return view('bills.status', ['bill' => $bill]);
-            // abort(404);
         }
 
         $invoice = (new Invoice)->amount( number_format($bill->total, 2, '.', ''));
