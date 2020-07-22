9012dd153dd0d49d5b796bd3d6f8581f42b7658e
Ahmed-Rabee <dev.ahmedrabee@gmail.com>
2020-07-22 14:55:42 +0200

    fix some words in lang

--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -4,7 +4,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-            <h5 class="modal-title" id="add_customer_ModalLabel">Add Customer</h5>
+            <h5 class="modal-title" id="add_customer_ModalLabel">{{ __('Add Customer') }}</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
             </button>
