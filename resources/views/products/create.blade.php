c49f0dacf3b62cab240397d58ed3fff7e9aeaf60
Ahmed-Rabee <dev.ahmedrabee@gmail.com>
2020-08-09 16:51:24 +0200

    add products page

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -2,6 +2,12 @@
 
 @section('title', __('Add Product'))
 
+@section('css_styles')
+  <link rel="stylesheet" href="{{ asset('css/dropzone.min.css') }}" />
+  <link rel="stylesheet" href="{{ asset('css/quill.snow.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/quill.bubble.css') }}">
+@endsection
+
 @section('content')
 
   <div class="row">
@@ -11,9 +17,9 @@
         <nav class="breadcrumb-container d-none d-sm-block d-lg-inline-block" aria-label="breadcrumb">
           <ol class="breadcrumb pt-0">
             <li class="breadcrumb-item">
-              <a href="{{ url('/') }}">{{ __('Home')}}</a>
+              <a href="{{ url('/') }}" title="{{ __('Home')}}">{{ __('Home')}}</a>
             </li>
-            <li class="breadcrumb-item" aria-current="page">{{ __('Product')}}</li>
+            <li class="breadcrumb-item"><a href="#" title="{{ __('Products')}}">{{ __('Products')}}</a></li>
             <li class="breadcrumb-item active" aria-current="page">{{ __('Add Product')}}</li>
           </ol>
         </nav>
@@ -22,19 +28,63 @@
     <div class="separator mb-5"></div>
   </div>
   <div class="row">
-  <div class="col-12">
-    <div class="card">
-      <div class="card-body">
-        <div class="table-responsive">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    <div class="col-12 col-sm-12">
+      <div class="card mb-4">
+        <div class="card-body">
+          <form>
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="inputEmail1">{{ __('Product Name')}}</label>
+                <input type="text" class="form-control" id="inputEmail1" placeholder="{{ __('Product Name')}}">
+              </div>
+              <div class="form-group col-md-6">
+                <label for="inputEmail2">{{ __('Category')}}</label>
+                <select id="inputEmail4" class="form-control">
+                  <option selected>{{ __('Choose ...')}}</option>
+                  <option value="1">Category 1</option>
+                  <option value="2">Category 2</option>
+                </select>
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="inputEmail3">{{ __('Price')}}</label>
+                <input type="tel" class="form-control" id="inputEmail3" placeholder="{{ __('Price')}}">
+              </div>
+              <div class="form-group col-md-6">
+                <label for="inputEmail3">{{ __('The maximum amount to request a product each time')}}</label>
+                <input type="number" class="form-control" id="inputEmail3" placeholder="0">
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="form-group col-12">
+                <label for="inputEmail3">{{ __('Description')}}</label>
+                <div class="html-editor" id="quillEditor"></div>
+              </div>
+              <div class="form-group col-12">
+                <label for="inputEmail4">{{ __('Product Images')}}</label>
+                <form action="/file-upload">
+                  <div class="dropzone"> </div>
+                </form>
+              </div>
+              <div class="form-group col-12">
+                <label for="inputEmail4">{{ __('Product Status')}}</label>
+                <div class="custom-switch custom-switch-primary mb-2 custom-switch-small">
+                  <input class="custom-switch-input" id="switchS2" type="checkbox" checked>
+                  <label class="custom-switch-btn" for="switchS2"></label>
+                </div>
+              </div>
+            </div>
+            <button type="submit" class="btn btn-primary d-inline-block mt-2">{{ __('Save')}}</button>
+            <button type="submit" class="btn btn-danger d-inline-block mt-2">{{ __('Cancel')}}</button>
+          </form>
         </div>
       </div>
     </div>
   </div>
-  </div>
+@endsection
+
+@section('footer-scripts')
+  <script src="{{ asset('js/dropzone.min.js') }}"></script>
+  <script src="{{ asset('js/quill.min.js') }}"></script>
 @endsection
