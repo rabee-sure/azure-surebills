e25953b38b367d02041bcb7ebb5e6eaad35e2b0a
Ahmed-Rabee <dev.ahmedrabee@gmail.com>
2020-08-10 09:11:47 +0200

    add store page

--- resources/views/products/store.blade.php
+++ resources/views/products/store.blade.php
@@ -3,10 +3,282 @@
 @section('title', __('Store'))
 
 @section('content')
-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+<div id="header_fixed">
+      <div class="container">
+        <div class="logo_client">
+          <a href="#" title="#"><img alt="Profile" src="../img/logoCN.png"></a>
+        </div><!-- logo_client -->
+        <div class="social_media">
+          <a href="#"><i class="simple-icon-social-facebook"></i></a>
+          <a href="#"><i class="simple-icon-social-twitter"></i></a>
+          <a href="#"><i class="simple-icon-social-instagram"></i></a>
+        </div><!-- social_media -->
+      </div><!-- container -->
+    </div><!-- Header_fixed -->
+
+    <div class="container my-5">
+      <div class="row">
+        <div class="col-12 mb-5">
+          <img class="social-header card-img" src="../img/social-header.jpg" />
+        </div>
+        <div class="col-12 col-lg-5 col-xl-4 col-left">
+          <img alt="Profile" src="../img/logoCN.png" class="img-thumbnail card-img social-profile-img">
+          <div class="card mb-4">
+            <div class="card-body">
+              <div class="text-center pt-4">
+                <p class="list-item-heading pt-2">Noura Cake</p>
+              </div>
+              <p class="mb-3">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              </p>
+              <p class="text-muted text-small mb-2">Contact</p>
+              <div class="social-icons">
+                <ul class="list-unstyled list-inline">
+                  <li class="list-inline-item">
+                    <a href="#"><i class="simple-icon-social-facebook"></i></a>
+                  </li>
+                  <li class="list-inline-item">
+                    <a href="#"><i class="simple-icon-social-twitter"></i></a>
+                  </li>
+                  <li class="list-inline-item">
+                    <a href="#"><i class="simple-icon-social-instagram"></i></a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-lg-7 col-xl-8 col-right">
+          <div class="row listing-card-container">
+            <div class="col-sm-6 col-lg-6 col-xl-4 col-12 mb-4">
+              <div class="card">
+                <div class="position-relative">
+                  <a href="product-client.html">
+                    <img class="card-img-top" src="../img/marble-cake-thumb.jpg" alt="Card image cap">
+                  </a>
+                </div>
+                <div class="card-body">
+                  <a href="product-client.html">
+                    <p class="list-item-heading mb-2 pt-1 font-weight-bold">Cheesecake</p>
+                    <p class="text-muted text-md mb-2 font-weight-normal"><b>545</b> SAR</p>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-lg-6 col-xl-4 col-12 mb-4">
+              <div class="card">
+                <div class="position-relative">
+                  <a href="product-client.html">
+                    <img class="card-img-top" src="../img/goose-breast-thumb.jpg" alt="Card image cap">
+                  </a>
+                </div>
+                <div class="card-body">
+                  <a href="product-client.html">
+                    <p class="list-item-heading mb-2 pt-1 font-weight-bold">Cheesecake</p>
+                    <p class="text-muted text-md mb-2 font-weight-normal"><b>545</b> SAR</p>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-lg-6 col-xl-4 col-12 mb-4">
+              <div class="card">
+                <div class="position-relative">
+                  <a href="product-client.html">
+                    <img class="card-img-top" src="../img/chocolate-cake-thumb.jpg" alt="Card image cap">
+                  </a>
+                </div>
+                <div class="card-body">
+                  <a href="product-client.html">
+                    <p class="list-item-heading mb-2 pt-1 font-weight-bold">Cheesecake</p>
+                    <p class="text-muted text-md mb-2 font-weight-normal"><b>545</b> SAR</p>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-lg-6 col-xl-4 col-12 mb-4">
+              <div class="card">
+                <div class="position-relative">
+                  <a href="product-client.html">
+                    <img class="card-img-top" src="../img/souffle-thumb.jpg" alt="Card image cap">
+                  </a>
+                </div>
+                <div class="card-body">
+                  <a href="product-client.html">
+                    <p class="list-item-heading mb-2 pt-1 font-weight-bold">Cheesecake</p>
+                    <p class="text-muted text-md mb-2 font-weight-normal"><b>545</b> SAR</p>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-lg-6 col-xl-4 col-12 mb-4">
+              <div class="card">
+                <div class="position-relative">
+                  <a href="product-client.html">
+                    <img class="card-img-top" src="../img/marble-cake-thumb.jpg" alt="Card image cap">
+                  </a>
+                </div>
+                <div class="card-body">
+                  <a href="product-client.html">
+                    <p class="list-item-heading mb-2 pt-1 font-weight-bold">Cheesecake</p>
+                    <p class="text-muted text-md mb-2 font-weight-normal"><b>545</b> SAR</p>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-lg-6 col-xl-4 col-12 mb-4">
+              <div class="card">
+                <div class="position-relative">
+                  <a href="product-client.html">
+                    <img class="card-img-top" src="../img/goose-breast-thumb.jpg" alt="Card image cap">
+                  </a>
+                </div>
+                <div class="card-body">
+                  <a href="product-client.html">
+                    <p class="list-item-heading mb-2 pt-1 font-weight-bold">Cheesecake</p>
+                    <p class="text-muted text-md mb-2 font-weight-normal"><b>545</b> SAR</p>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-lg-6 col-xl-4 col-12 mb-4">
+              <div class="card">
+                <div class="position-relative">
+                  <a href="product-client.html">
+                    <img class="card-img-top" src="../img/chocolate-cake-thumb.jpg" alt="Card image cap">
+                  </a>
+                </div>
+                <div class="card-body">
+                  <a href="product-client.html">
+                    <p class="list-item-heading mb-2 pt-1 font-weight-bold">Cheesecake</p>
+                    <p class="text-muted text-md mb-2 font-weight-normal"><b>545</b> SAR</p>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-lg-6 col-xl-4 col-12 mb-4">
+              <div class="card">
+                <div class="position-relative">
+                  <a href="product-client.html">
+                    <img class="card-img-top" src="../img/souffle-thumb.jpg" alt="Card image cap">
+                  </a>
+                </div>
+                <div class="card-body">
+                  <a href="product-client.html">
+                    <p class="list-item-heading mb-2 pt-1 font-weight-bold">Cheesecake</p>
+                    <p class="text-muted text-md mb-2 font-weight-normal"><b>545</b> SAR</p>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-lg-6 col-xl-4 col-12 mb-4">
+              <div class="card">
+                <div class="position-relative">
+                  <a href="product-client.html">
+                    <img class="card-img-top" src="../img/marble-cake-thumb.jpg" alt="Card image cap">
+                  </a>
+                </div>
+                <div class="card-body">
+                  <a href="product-client.html">
+                    <p class="list-item-heading mb-2 pt-1 font-weight-bold">Cheesecake</p>
+                    <p class="text-muted text-md mb-2 font-weight-normal"><b>545</b> SAR</p>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-lg-6 col-xl-4 col-12 mb-4">
+              <div class="card">
+                <div class="position-relative">
+                  <a href="product-client.html">
+                    <img class="card-img-top" src="../img/goose-breast-thumb.jpg" alt="Card image cap">
+                  </a>
+                </div>
+                <div class="card-body">
+                  <a href="product-client.html">
+                    <p class="list-item-heading mb-2 pt-1 font-weight-bold">Cheesecake</p>
+                    <p class="text-muted text-md mb-2 font-weight-normal"><b>545</b> SAR</p>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-lg-6 col-xl-4 col-12 mb-4">
+              <div class="card">
+                <div class="position-relative">
+                  <a href="product-client.html">
+                    <img class="card-img-top" src="../img/chocolate-cake-thumb.jpg" alt="Card image cap">
+                  </a>
+                </div>
+                <div class="card-body">
+                  <a href="product-client.html">
+                    <p class="list-item-heading mb-2 pt-1 font-weight-bold">Cheesecake</p>
+                    <p class="text-muted text-md mb-2 font-weight-normal"><b>545</b> SAR</p>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-lg-6 col-xl-4 col-12 mb-4">
+              <div class="card">
+                <div class="position-relative">
+                  <a href="product-client.html">
+                    <img class="card-img-top" src="../img/souffle-thumb.jpg" alt="Card image cap">
+                  </a>
+                </div>
+                <div class="card-body">
+                  <a href="product-client.html">
+                    <p class="list-item-heading mb-2 pt-1 font-weight-bold">Cheesecake</p>
+                    <p class="text-muted text-md mb-2 font-weight-normal"><b>545</b> SAR</p>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-lg-6 col-xl-4 col-12 mb-4">
+              <div class="card">
+                <div class="position-relative">
+                  <a href="product-client.html">
+                    <img class="card-img-top" src="../img/marble-cake-thumb.jpg" alt="Card image cap">
+                  </a>
+                </div>
+                <div class="card-body">
+                  <a href="product-client.html">
+                    <p class="list-item-heading mb-2 pt-1 font-weight-bold">Cheesecake</p>
+                    <p class="text-muted text-md mb-2 font-weight-normal"><b>545</b> SAR</p>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-lg-6 col-xl-4 col-12 mb-4">
+              <div class="card">
+                <div class="position-relative">
+                  <a href="product-client.html">
+                    <img class="card-img-top" src="../img/goose-breast-thumb.jpg" alt="Card image cap">
+                  </a>
+                </div>
+                <div class="card-body">
+                  <a href="product-client.html">
+                    <p class="list-item-heading mb-2 pt-1 font-weight-bold">Cheesecake</p>
+                    <p class="text-muted text-md mb-2 font-weight-normal"><b>545</b> SAR</p>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-lg-6 col-xl-4 col-12 mb-4">
+              <div class="card">
+                <div class="position-relative">
+                  <a href="product-client.html">
+                    <img class="card-img-top" src="../img/chocolate-cake-thumb.jpg" alt="Card image cap">
+                  </a>
+                </div>
+                <div class="card-body">
+                  <a href="product-client.html">
+                    <p class="list-item-heading mb-2 pt-1 font-weight-bold">Cheesecake</p>
+                    <p class="text-muted text-md mb-2 font-weight-normal"><b>545</b> SAR</p>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
 @endsection
