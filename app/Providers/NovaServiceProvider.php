e9aa68c25c6eca3b133e05cba04e1a13f20d3e40
Essam Abdel-Sabour <me@essam.ws>
2020-09-24 11:52:17 +0200

    add alaa to admins

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -50,7 +50,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
                 'faisal@toot.im',
                 'eabdelsabour@sure.com.sa',
                 'aghanem@sure.com.sa',
-                'mjarad@sure.com.sa',
+                'ayoussef@sure.com.sa',
             ]);
         });
     }
