6ec7ea004ebc7a2c33ab1501584fc2ac4ad1713d
Essam Abdel-Sabour <me@essam.ws>
2020-09-29 14:34:24 +0200

    improvments

--- app/Nova/Bill.php
+++ app/Nova/Bill.php
@@ -120,6 +120,7 @@ class Bill extends Resource
     public function fields(Request $request)
     {
         return [
+
             Text::make(__('Name'), function () {
                 return __('Bill').' '.  $this->number  .'-'. $this->customer_name;
             }),
@@ -128,17 +129,7 @@ class Bill extends Resource
                 'paid' => 'success',
                 'canceled' => 'warning',
                 'expired' => 'danger',
-            ]),         
-            Select::make(__('Payment Method'), 'payment_method')->options([
-                'credit' => 'credit',
-                'stc' => 'stc',
-                'apple' => 'apple',
             ]),
-
-            Number::make(__('Total'), 'total')->min(1)->step(0.1),
-            Number::make( __('Payment Fees'), 'payment_fees')->min(1)->step(0.1),
-            Number::make(__('discount'), 'discount')->min(1)->step(0.1)->onlyOnDetail(),
-            Number::make(__('Tax'), 'vat')->min(1)->step(0.1)->onlyOnDetail(),
             DateTime::make(__('Created At'), 'created_at')->exceptOnForms(),
             BelongsTo::make(__('User'), 'user', User::class),
             BelongsTo::make(__('Customer'), 'customer', Customer::class)->onlyOnDetail(),
@@ -150,6 +141,24 @@ class Bill extends Resource
 
             Boolean::make(__('Send Email'), 'send_email')->onlyOnDetail(),
             Boolean::make(__('Send Sms'), 'send_sms')->onlyOnDetail(),
+
+            new Panel(__('Payment Details'), function(){
+                return [
+                    Select::make(__('Payment Method'), 'payment_method_details')->options([
+                        'credit' => 'credit',
+                        'stc' => 'stc',
+                        'apple' => 'apple',
+                    ]),
+
+                    Number::make(__('Sub Total'), 'sub_total')->min(1)->step(0.1)->onlyOnDetail(),
+                    Number::make(__('Discount'), 'discount')->min(1)->step(0.1)->onlyOnDetail(),
+                    Number::make(__('Tax'), 'vat')->min(1)->step(0.1)->onlyOnDetail(),
+                    Number::make(__('Total'), 'total')->min(1)->step(0.1),
+                    Number::make( __('Payment Fees'), 'payment_fees')->min(1)->step(0.1)->onlyOnDetail(),
+                    Number::make( __('Payment Fees VAT'), 'payment_fees_vat')->min(1)->step(0.1)->onlyOnDetail(),
+                    Number::make( __('Due to client'), 'due_to_client')->min(1)->step(0.1)->onlyOnDetail(),
+                ];
+            }),
         ];
     }
 
