ae7a67ed7143bbb64ec03536c73bc124e465d6b5
abdullah ghanem <aghanem@sure.com.sa>
2020-10-07 11:53:51 +0200

    fix localezation dropzone @SBV2-101

--- resources/views/components/dropzone.blade.php
+++ resources/views/components/dropzone.blade.php
@@ -1,4 +1,5 @@
 <div class="dropzone">
+    <div class="dz-message" data-dz-message><span>{{ __('Drop files here to upload') }}</span></div>
 </div>
 
 
@@ -40,7 +41,7 @@
             }
           @endif
         },
-        thumbnailWidth: 160,
+        thumbnailWidth: 200,
         previewTemplate: '<div class="dz-preview dz-file-preview mb-3"><div class="d-flex flex-row "><div class="p-0 w-30 position-relative"><div class="dz-error-mark"><span><i></i></span></div><div class="dz-success-mark"><span><i></i></span></div><div class="preview-container"><img data-dz-thumbnail class="img-thumbnail border-0" /><i class="simple-icon-doc preview-icon" ></i></div></div><div class="pl-3 pt-2 pr-2 pb-1 w-70 dz-details position-relative"><div><span data-dz-name></span></div><div class="text-primary text-extra-small" data-dz-size /><div class="dz-progress"><span class="dz-upload" data-dz-uploadprogress></span></div><div class="dz-error-message"><span data-dz-errormessage></span></div></div></div><a href="#/" class="remove" data-dz-remove><i class="glyph-icon simple-icon-trash"></i></a></div>'
       });
     }
