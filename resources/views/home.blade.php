1a2b488ae4700f4e0182f787e25ee5025b6d8626
Ahmed-Rabee <dev.ahmedrabee@gmail.com>
2020-10-11 15:52:30 +0200

    fix block in homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,7 +39,7 @@
                 </div>
               </a>
             </div>
-            <div class="col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3">
+            <div class="col-6 col-sm-6 col-md-3 col-lg-3 col-xl-3">
               <a href="/bills?statuses[]=paid&dont_update_statuses=true" class="card mb-4">
                 <div class="card-body text-center">
                   <div class="statistic_icon settlements_icon"></div>
