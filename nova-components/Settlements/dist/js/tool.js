aa90c89564b57e2b11babd487466e923e1ac89df
abdullah ghanem <aghanem@sure.com.sa>
2020-10-21 16:18:14 +0200

    transfer page update transaction details @SBV2-121

--- nova-components/Settlements/dist/js/tool.js
+++ nova-components/Settlements/dist/js/tool.js
@@ -60153,78 +60153,102 @@ var render = function() {
   return _c(
     "div",
     [
+      _vm.row.id
+        ? _c(
+            "Row",
+            { staticClass: "expand-row" },
+            [
+              _c("Col", { attrs: { span: "24" } }, [
+                _c("span", { staticClass: "expand-key" }, [_vm._v("Id: ")]),
+                _vm._v(" "),
+                _c("span", { staticClass: "expand-value" }, [
+                  _vm._v(_vm._s(_vm.row.id))
+                ])
+              ])
+            ],
+            1
+          )
+        : _vm._e(),
+      _vm._v(" "),
       _c(
         "Row",
         { staticClass: "expand-row" },
         [
-          _c("Col", { attrs: { span: "24" } }, [
-            _c("span", { staticClass: "expand-key" }, [_vm._v("Id: ")]),
-            _vm._v(" "),
-            _c("span", { staticClass: "expand-value" }, [
-              _vm._v(_vm._s(_vm.row.id))
-            ])
-          ])
+          _vm.row.balance
+            ? _c("Col", { attrs: { span: "8" } }, [
+                _c("span", { staticClass: "expand-key" }, [
+                  _vm._v("balance: ")
+                ]),
+                _vm._v(" "),
+                _c("span", { staticClass: "expand-value" }, [
+                  _vm._v(_vm._s(_vm.row.balance))
+                ])
+              ])
+            : _vm._e(),
+          _vm._v(" "),
+          _vm.row.card
+            ? _c("Col", { attrs: { span: "8" } }, [
+                _c("span", { staticClass: "expand-key" }, [_vm._v("CARD : ")]),
+                _vm._v(" "),
+                _c("span", { staticClass: "expand-value" }, [
+                  _vm._v(_vm._s(_vm.row.card))
+                ])
+              ])
+            : _vm._e(),
+          _vm._v(" "),
+          _vm.row.card_brand
+            ? _c("Col", { attrs: { span: "8" } }, [
+                _c("span", { staticClass: "expand-key" }, [
+                  _vm._v("CARD BRAND: ")
+                ]),
+                _vm._v(" "),
+                _c("span", { staticClass: "expand-value" }, [
+                  _vm._v(_vm._s(_vm.row.card_brand))
+                ])
+              ])
+            : _vm._e()
         ],
         1
       ),
       _vm._v(" "),
       _c(
         "Row",
-        { staticClass: "expand-row" },
         [
-          _c("Col", { attrs: { span: "8" } }, [
-            _c("span", { staticClass: "expand-key" }, [_vm._v("balance: ")]),
-            _vm._v(" "),
-            _c("span", { staticClass: "expand-value" }, [
-              _vm._v(_vm._s(_vm.row.balance))
-            ])
-          ]),
+          _vm.row.auth_id
+            ? _c("Col", { attrs: { span: "8" } }, [
+                _c("span", { staticClass: "expand-key" }, [
+                  _vm._v("Auth id: ")
+                ]),
+                _vm._v(" "),
+                _c("span", { staticClass: "expand-value" }, [
+                  _vm._v("《" + _vm._s(_vm.row.auth_id) + "》")
+                ])
+              ])
+            : _vm._e(),
           _vm._v(" "),
-          _c("Col", { attrs: { span: "8" } }, [
-            _c("span", { staticClass: "expand-key" }, [_vm._v("CARD : ")]),
-            _vm._v(" "),
-            _c("span", { staticClass: "expand-value" }, [
-              _vm._v(_vm._s(_vm.row.card))
-            ])
-          ]),
+          _vm.row.reference
+            ? _c("Col", { attrs: { span: "8" } }, [
+                _c("span", { staticClass: "expand-key" }, [
+                  _vm._v("Reference: ")
+                ]),
+                _vm._v(" "),
+                _c("span", { staticClass: "expand-value" }, [
+                  _vm._v(_vm._s(_vm.row.reference))
+                ])
+              ])
+            : _vm._e(),
           _vm._v(" "),
-          _c("Col", { attrs: { span: "8" } }, [
-            _c("span", { staticClass: "expand-key" }, [_vm._v("CARD BRAND: ")]),
-            _vm._v(" "),
-            _c("span", { staticClass: "expand-value" }, [
-              _vm._v(_vm._s(_vm.row.card_brand))
-            ])
-          ])
-        ],
-        1
-      ),
-      _vm._v(" "),
-      _c(
-        "Row",
-        [
-          _c("Col", { attrs: { span: "8" } }, [
-            _c("span", { staticClass: "expand-key" }, [_vm._v("Auth id: ")]),
-            _vm._v(" "),
-            _c("span", { staticClass: "expand-value" }, [
-              _vm._v("《" + _vm._s(_vm.row.auth_id) + "》")
-            ])
-          ]),
-          _vm._v(" "),
-          _c("Col", { attrs: { span: "8" } }, [
-            _c("span", { staticClass: "expand-key" }, [_vm._v("Reference: ")]),
-            _vm._v(" "),
-            _c("span", { staticClass: "expand-value" }, [
-              _vm._v(_vm._s(_vm.row.reference))
-            ])
-          ]),
-          _vm._v(" "),
-          _c("Col", { attrs: { span: "8" } }, [
-            _c("span", { staticClass: "expand-key" }, [_vm._v("Receipt: ")]),
-            _vm._v(" "),
-            _c("span", { staticClass: "expand-value" }, [
-              _vm._v(_vm._s(_vm.row.receipt))
-            ])
-          ])
+          _vm.row.receipt
+            ? _c("Col", { attrs: { span: "8" } }, [
+                _c("span", { staticClass: "expand-key" }, [
+                  _vm._v("Receipt: ")
+                ]),
+                _vm._v(" "),
+                _c("span", { staticClass: "expand-value" }, [
+                  _vm._v(_vm._s(_vm.row.receipt))
+                ])
+              ])
+            : _vm._e()
         ],
         1
       )
