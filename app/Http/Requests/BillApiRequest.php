1822239f5f1e1da8e3474f6d3d00c3ac7976c586
Essam Abdel-Sabour <me@essam.ws>
2020-11-10 13:15:53 +0200

    a

--- app/Http/Requests/BillApiRequest.php
+++ app/Http/Requests/BillApiRequest.php
@@ -27,6 +27,10 @@ class BillApiRequest extends FormRequest
             $mobile = (int) $mobile;
             $this->merge(['customer_mobile'=> $mobile]);
         }
+
+        if (!$this->has('customer_mobile')) {
+            $this->is_redirect = false;
+        }
     }
 
     /**
