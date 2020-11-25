93703aaf66d83f20dd2a3152c1b36b73d89f3e36
abdullah ghanem <aghanem@sure.com.sa>
2020-11-25 14:09:58 +0200

    add TestController to .gitignore

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('test', 'TestController@test');
+// Route::get('test', 'TestController@test');
 
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
