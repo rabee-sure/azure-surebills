95fc5ae1ccb836321452fa1690c6ab15b734d5ca
Amr Aly Abdelaty <AAbdelaty@sure.com.sa>
2020-12-01 15:26:07 +0200

    solve webhook

--- app/Helpers/PaymentHelper.php
+++ app/Helpers/PaymentHelper.php
@@ -27,6 +27,7 @@ class PaymentHelper
 
     public static function handlePaymentResponse($invoice, $orderBody, $billDetail)
     {
+        dd($invoice);
         if($billDetail['bill']['status'] != 'paid')
         {
             $orderResponseJson['id'] = $orderBody->id;
