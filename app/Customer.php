3b18417937094fb71ad9dee9d78ab2fa9e65233c
Amr Aly Abdelaty <Aabdelaty@sure.com.sa>
2020-12-03 14:31:44 +0200

    solve issue num: SBV2-191

--- app/Customer.php
+++ app/Customer.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    protected $fillable = ['name', 'email', 'mobile', 'Customer', 'user_id'];
+    protected $fillable = ['name', 'email', 'mobile', 'Customer', 'user_id', 'notes'];
 
     /**
      * Get bills.
