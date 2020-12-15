2cbe55fa6e2ffc15477853c893e945d45a0f9ef1
abdullah ghanem <aghanem@sure.com.sa>
2020-12-15 15:54:20 +0200

    create command for compare excel sheets

---
+++ app/Console/Commands/CompareExcelSheet.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\PaymentLog;
+use App\Imports\HyperPayImport;
+use App\Transaction;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+use Maatwebsite\Excel\Facades\Excel;
+
+class CompareExcelSheet extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'compare:excel_sheet';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'compare excel sheets';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $path = $this->ask('What is your file name in "app/public"?');
+
+        $excel_file = storage_path('app/public/'.$path);
+
+        if(Storage::disk('public')->has($path)){
+            $data = Excel::toCollection(new HyperPayImport, $excel_file)[0];
+
+            $uniqueids = $data->pluck('uniqueid')
+                ->map(fn($item, $key) => str_replace(["'", '"'], '', $item));
+            $payment_log_ids = PaymentLog::whereIn('results->bill->id', $uniqueids->toArray())
+                ->get()
+                ->pluck('results.bill.id');
+
+            $diff = $uniqueids->diff($payment_log_ids);
+
+            $this->info('there is '. $diff->count() . ' recourd dont saved in database');
+            $this->info('-------------------------');
+            foreach ($diff->all() as $value) {
+                $this->line($value);
+            }
+        }else{
+            $this->error('Something went wrong! cant find file');
+        }
+    }
+}
