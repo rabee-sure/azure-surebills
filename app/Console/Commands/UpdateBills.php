c2ff062d9b25e7df1d554351b8b4f365a6285f55
abdullah ghanem <aghanem@sure.com.sa>
2021-01-11 10:15:57 +0200

    create command for update bills fees and Vat

---
+++ app/Console/Commands/UpdateBills.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Application;
+use App\Bill;
+use App\Events\BillStatusUpdated;
+use App\Events\UserCreated;
+use App\Jobs\MakeTransactionsForSureBills;
+use App\Transaction;
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UpdateBills extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'bills:update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update Bills';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Bill::where('payment_surebills_fees_vat', null)->paid()->chunk(500, function($bills)
+        {
+            foreach ($bills as $bill) {
+                $payment_log = $bill->success_payment;
+
+                if($payment_log){
+                    $bill = $bill;
+                    $percentage = $bill->getPercentage($payment_log);
+                    $fixed = $bill->getFixed($payment_log);
+
+                    $bill->settled = false;
+                    $bill->pricing_fees_details = $percentage.'%,'. $fixed;
+                    $bill->payment_fees = $bill->total * ($percentage / 100) + $fixed;
+                    $bill->payment_fees_vat = $bill->payment_fees * (Transaction::VAT_PERCENTAGE / 100);
+
+                    $payment_surebills = $this->paymentSurebillsFees($bill, $payment_log);
+                    $bill->payment_surebills_fees = $payment_surebills['fees'];
+                    $bill->payment_surebills_fees_vat = $payment_surebills['fees_vat'];
+
+                    $payment_channel = $this->paymentChannelFees($bill, $payment_log);
+                    $bill->payment_channel_fees = $payment_channel['fees'];
+                    $bill->payment_channel_fees_vat = $payment_channel['fees_vat'];
+                    $bill->save();
+
+                    $this->info("update bill id: {$bill->id}");
+                }
+            }
+        });
+    }
+
+
+    protected function paymentSurebillsFees($bill, $log):Array
+    {
+        if(isset($bill->application) && isset($bill->application->channel)){
+            $percentage = $bill->getPercentage($log, true);
+            $fixed = $bill->getFixed($log, true);
+
+            $payment_fees = $bill->total * ($percentage / 100) + $fixed;
+            $payment_fees_vat = $payment_fees * (Transaction::VAT_PERCENTAGE / 100);
+        }else{
+            $payment_fees = $bill->payment_fees;
+            $payment_fees_vat = $bill->payment_fees_vat;
+        }
+
+        return [
+            'fees' => $payment_fees,
+            'fees_vat' => $payment_fees_vat,
+        ];
+    }
+
+    protected function paymentChannelFees($bill, $log):Array
+    {
+        if(isset($bill->application) && isset($bill->application->channel)){
+            $percentage = $bill->getPercentage($log, true);
+            $fixed = $bill->getFixed($log, true);
+
+            $p_fees = $bill->total * ($percentage / 100) + $fixed;
+            $p_fees_vat = $p_fees * (Transaction::VAT_PERCENTAGE / 100);
+
+            $payment_fees = $bill->payment_fees - $p_fees;
+            $payment_fees_vat = $bill->payment_fees_vat - $p_fees_vat;
+        }else{
+            $payment_fees = null;
+            $payment_fees_vat = null;
+        }
+
+        return [
+            'fees' => $payment_fees,
+            'fees_vat' => $payment_fees_vat,
+        ];
+    }
+}
