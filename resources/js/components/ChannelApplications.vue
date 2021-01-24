6712ba75a8d7e4b12f8b8d1c0bc607d2eadbb3fa
abdullah ghanem <aghanem@sure.com.sa>
2021-01-24 14:46:05 +0200

    create new rule for Decimal @SBV2-262

--- resources/js/components/ChannelApplications.vue
+++ resources/js/components/ChannelApplications.vue
@@ -216,7 +216,7 @@
               <div class="form-group row">
                 <label class="col-md-4 col-form-label">{{ __('Mada Fixed') }}<span class="requirement">*</span></label>
                 <div class="col-md-8">
-                  <input :class="{'is-invalid': haveError('mada_fixed', 2) }" type="number" class="form-control" name="mada_fixed" @keyup.enter="update" v-model="editForm.mada_fixed">
+                  <input :class="{'is-invalid': haveError('mada_fixed', 2) }" type="number" class="form-control" name="mada_fixed" @keyup.enter="update" v-model="editForm.mada_fixed" step="0.01">
     
                     <div class="invalid-feedback" v-if="haveError('mada_fixed', 2)">
                         {{errorMessage('mada_fixed', 2)}}
@@ -228,7 +228,7 @@
               <div class="form-group row">
                 <label class="col-md-4 col-form-label">{{ __('Mada Percentage') }}<span class="requirement">*</span></label>
                 <div class="col-md-8">
-                  <input :class="{'is-invalid': haveError('mada_percentage', 2) }" type="number" class="form-control" name="mada_percentage" @keyup.enter="update" v-model="editForm.mada_percentage">
+                  <input :class="{'is-invalid': haveError('mada_percentage', 2) }" type="number" class="form-control" name="mada_percentage" @keyup.enter="update" v-model="editForm.mada_percentage" step="0.01">
     
                     <div class="invalid-feedback" v-if="haveError('mada_percentage', 2)">
                         {{errorMessage('mada_percentage', 2)}}
@@ -240,7 +240,7 @@
               <div class="form-group row">
                 <label class="col-md-4 col-form-label">{{ __('Credit Cards Fixed') }}<span class="requirement">*</span></label>
                 <div class="col-md-8">
-                  <input :class="{'is-invalid': haveError('credit_cards_fixed', 2) }" type="number" class="form-control" name="credit_cards_fixed" @keyup.enter="update" v-model="editForm.credit_cards_fixed">
+                  <input :class="{'is-invalid': haveError('credit_cards_fixed', 2) }" type="number" class="form-control" name="credit_cards_fixed" @keyup.enter="update" v-model="editForm.credit_cards_fixed" step="0.01">
     
                     <div class="invalid-feedback" v-if="haveError('credit_cards_fixed', 2)">
                         {{errorMessage('credit_cards_fixed', 2)}}
@@ -252,7 +252,7 @@
               <div class="form-group row">
                 <label class="col-md-4 col-form-label">{{ __('Credit Cards Percentage') }}<span class="requirement">*</span></label>
                 <div class="col-md-8">
-                  <input :class="{'is-invalid': haveError('credit_cards_percentage', 2) }"  type="number" class="form-control" name="credit_cards_percentage" @keyup.enter="update" v-model="editForm.credit_cards_percentage">
+                  <input :class="{'is-invalid': haveError('credit_cards_percentage', 2) }"  type="number" class="form-control" name="credit_cards_percentage" @keyup.enter="update" v-model="editForm.credit_cards_percentage" step="0.01">
     
                     <div class="invalid-feedback" v-if="haveError('credit_cards_percentage', 2)">
                         {{errorMessage('credit_cards_percentage', 2)}}
