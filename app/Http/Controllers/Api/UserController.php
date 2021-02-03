4f9774cd4839a341a4dfc48918db66820bb90038
abdullah ghanem <aghanem@sure.com.sa>
2021-02-03 16:20:13 +0200

    create new api for register user with create application

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -29,7 +29,7 @@ class UserController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function register(Request $request)
+    public function registerFandaqah(Request $request)
     {
         $validatedData = $request->validate([
             'business_name' => ['required', 'string', 'max:100'],
@@ -92,5 +92,61 @@ class UserController extends Controller
         $application->save();
 
         return true;
+    }
+
+
+    /**
+     * Store a new User.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $user = User::whereEmail($request->email)->orWhere('mobile', $request->mobile)->first();
+        if($user == null){
+            $validatedData = $request->validate([
+                'business_name' => ['required', 'string', 'max:100'],
+                'name' => ['required', 'string', 'max:50'],
+                'email' => ['required', 'string', 'email', 'max:50', 'unique:users,email'],
+                'mobile' => ['unique:users'],
+            ]);
+
+            $user = new User;
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->mobile = $request->mobile;
+            $user->business_name_en = $request->business_name;
+            $user->password = $request->email . $request->name;
+            $user->save();
+            event(new UserCreated($user));  
+        }
+
+        $validatedData = $request->validate([
+            'redirect_url' => ['required'],
+            'webhook_url' => ['required'],
+            'application_name' => ['required'],
+            'channel_id' => ['nullable'],
+        ]);
+
+        $application = new Application;
+        $application->user_id = $user->id;
+        $application->name = $request->application_name;
+        $application->secret = Str::random(20);
+        $application->redirect = $request->redirect_url;
+        $application->webhook_url = $request->webhook_url;
+        $application->webhook_secret = Str::random(20);
+        $application->channel_id = $request->channel_id;
+        $application->mada_percentage = 3;
+        $application->credit_cards_percentage = 3;
+        $application->mada_fixed = 0;
+        $application->credit_cards_fixed = 0;
+        $application->save();
+
+        return [
+            'client_id'      => $application->id,
+            'secret'         => $application->secret,
+            'webhook_secret' => $application->webhook_secret
+        ];
     }
 }
