c86627200da6f02b0bc101669ec56cb34eb20c16
Ahmed-Rabee <dev.ahmedrabee@gmail.com>
2021-02-04 09:09:04 +0200

    fix payment page

--- resources/views/bills/payment_page.blade.php
+++ resources/views/bills/payment_page.blade.php
@@ -9,7 +9,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <title>{{ __('Bill') . ' ' . $bill->number }} - SureBills</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
     <!-- App Css -->
     <link rel="stylesheet" href="/payment_page/css/app.css">
     <!-- Arabic Font -->
