cda9abb8516c3f93cbac84fb38f4b48662611fb8
abdullah ghanem <aghanem@sure.com.sa>
2021-02-07 10:19:10 +0200

    localization issue in the payment notification mail @SBV2-313

--- resources/views/emails/bills/paid_to_customer.blade.php
+++ resources/views/emails/bills/paid_to_customer.blade.php
@@ -230,25 +230,25 @@
           <p>{{ $bill->user->business_address }}</p>
           <small>{{  $bill->user->business_mobile }}</small>
         </div><!-- block_1 -->
-              @if($bill->status == 'expired')
-                <div class="alert alert-danger" role="alert">
-                  {{ __('this bill has been expired', ['number' => $bill->number ]) }}
-                </div>
-              @endif
-              @if($bill->status == 'paid')
-                <div class="alert alert-success" role="alert">
-                  @if ($bill->depositTransaction)
-                    {{ __('Paid') }} - {{ $bill->depositTransaction->card_brand }} {{ $bill->depositTransaction->card }} {{ $bill->depositTransaction->receipt }}
-                  @else
-                  {{ __('this bill has been successfully', ['number' => $bill->number ]) }}
-                  @endif
-                </div>
-              @endif
-              @if($bill->status == 'canceled')
-                <div class="alert alert-danger" role="alert">
-                  {{ __('this bill has been canceled', ['number' => $bill->number ]) }}
-                </div>
-              @endif
+        @if($bill->status == 'expired')
+          <div class="alert alert-danger" role="alert">
+            {{ __('this bill has been expired', ['number' => $bill->number ]) }}
+          </div>
+        @endif
+        @if($bill->status == 'paid')
+          <div class="alert alert-success" role="alert">
+            @if ($bill->depositTransaction)
+              {{ __('Paid') }} - {{ $bill->depositTransaction->card_brand }} {{ $bill->depositTransaction->card }} {{ $bill->depositTransaction->receipt }}
+            @else
+            {{ __('this bill has been successfully', ['number' => $bill->number ]) }}
+            @endif
+          </div>
+        @endif
+        @if($bill->status == 'canceled')
+          <div class="alert alert-danger" role="alert">
+            {{ __('this bill has been canceled', ['number' => $bill->number ]) }}
+          </div>
+        @endif
         <div class="block_2">
           <span>
             {{ __('Due on') }} {{ $bill->dateLocalization()}}
