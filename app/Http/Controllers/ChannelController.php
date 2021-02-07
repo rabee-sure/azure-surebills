d66471f753e8f1be54844f9de7e965f3070e3b5a
abdullah ghanem <aghanem@sure.com.sa>
2021-02-07 11:36:43 +0200

    update

--- app/Http/Controllers/ChannelController.php
+++ app/Http/Controllers/ChannelController.php
@@ -81,13 +81,15 @@ class ChannelController extends Controller
      * @param  \App\Channel  $channels
      * @return \Illuminate\Http\Response
      */
-    public function update(ChannelUpdateRequest $request, Channel $channels)
+    public function update(ChannelUpdateRequest $request, Channel $channel)
     {
-        $channels->name = $request->name;
-        $channels->email = $request->email;
-        $channels->mobile = $request->mobile;
-        $channels->notes = $request->notes;
-        $channels->save();
+        $this->authorize('update', $channel);
+
+        $channel->name = $request->name;
+        $channel->email = $request->email;
+        $channel->mobile = $request->mobile;
+        $channel->notes = $request->notes;
+        $channel->save();
 
         return redirect()->route('channels.index');
     }
