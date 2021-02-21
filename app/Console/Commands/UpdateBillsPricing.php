e859d439b0c6ab2df93761cc544189dc67dd5719
abdullah ghanem <aghanem@sure.com.sa>
2021-02-21 17:35:29 +0200

    create-bills-pricing

---
+++ app/Console/Commands/UpdateBillsPricing.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Application;
+use App\Models\Bill;
+use App\Events\BillStatusUpdated;
+use App\Events\UserCreated;
+use App\Jobs\MakeTransactionsForSureBills;
+use App\Models\Transaction;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UpdateBillsPricing extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'bills:pricing';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update Bills pricing';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Bill::where('pricing', null)->paid()->chunk(500, function($bills)
+        {
+            foreach ($bills as $bill) {
+                $payment_log = $bill->success_payment;
+
+                if($payment_log){
+                    $bill = $bill;
+                    $percentage = $bill->getPercentage($payment_log);
+                    $fixed = $bill->getFixed($payment_log);
+
+                    $bill->pricing = [
+                        'type' => $this->getType($bill),
+                        'fees_percentage' => $percentage,
+                        'fees_fixed' => $fixed,
+                        'channel_fees_percentage' => $this->getType($bill) == 'channel' ?$bill->getPercentage($payment_log, true) : null,
+                        'channel_fees_fixed' =>  $this->getType($bill) == 'channel' ? $bill->getFixed($payment_log, true) : null,
+                        'vat_percentage' => Transaction::VAT_PERCENTAGE,
+                    ];
+
+                    $bill->save();
+
+                    $this->info("update bill id: {$bill->id}");
+                }
+            }
+        });
+    }
+    
+    protected function getType($bill)
+    {
+        if($bill->application_id){
+            if($bill->application->channel_id){
+                return 'channel';
+            }
+            else{
+                return 'application';
+            }
+        }else{
+            return 'user';
+        }
+    }
+}
--- app/Models/Bill.php
+++ app/Models/Bill.php
@@ -51,6 +51,7 @@ class Bill extends Model
 
         'expiry_minutes',
         'expiry_hours',
+        'pricing',
         'pricing_fees_details',
         'is_redirect',
 
@@ -70,6 +71,7 @@ class Bill extends Model
         'due_date' => 'datetime:Y-m-d',
         'canceled_at' => 'datetime',
         'paid_at' => 'datetime',
+        'pricing' => 'array',
     ];
 
     /**
