10fd70558fe960d662d75d8d516f9951896458d6
Essam Abdel-Sabour <me@essam.ws>
2021-02-24 06:08:52 +0200

    fix issue

--- app/Listeners/Webhook/SaveWebhookSucceededLog.php
+++ app/Listeners/Webhook/SaveWebhookSucceededLog.php
@@ -3,9 +3,10 @@
 namespace App\Listeners\Webhook;
 
 use App\Models\Bill;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
+use App\Models\WebhookLog;
 use Multicaret\Unifonic\UnifonicFacade;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Spatie\WebhookServer\Events\WebhookCallSucceededEvent;
 
 class SaveWebhookSucceededLog implements ShouldQueue
