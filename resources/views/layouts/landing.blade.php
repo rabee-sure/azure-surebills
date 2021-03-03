4a7e3278fed146117eddf9bc712f139d1041a389
abdullah ghanem <aghanem@sure.com.sa>
2021-03-03 15:14:31 +0200

    update scripts

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -79,7 +79,7 @@
     <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.2/CFInstall.min.js"></script>
     <script>window.attachEvent("onload",function(){CFInstall.check({mode:"overlay"})})</script>
     <![endif]-->
-    <script type="text/javascript" src="/landing/dist/js/landing.js"></script>
+    <script type="text/javascript" src="{{ asset('js/landing.js') }}"></script>
 
     @stack('footer-scripts')
 </body>
