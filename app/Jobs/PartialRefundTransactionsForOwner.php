2abb961695ce01431dfcc1421c5e5d7d980bce97
abdullah ghanem <aghanem@sure.com.sa>
2021-04-20 13:24:54 +0200

    update transactions order @SBV2-359

--- app/Jobs/PartialRefundTransactionsForOwner.php
+++ app/Jobs/PartialRefundTransactionsForOwner.php
@@ -43,6 +43,17 @@ class PartialRefundTransactionsForOwner
         $payment_fees = $this->amount * ($percentage / 100) + $fixed;
         $payment_fees_vat = $payment_fees * ($this->bill->pricing['vat_percentage'] / 100);
 
+
+        $transaction = new Transaction;
+        $transaction->user_id     = $this->bill->user_id;
+        $transaction->bill_id     = $this->bill->id;
+        $transaction->type        = 'debit';
+        $transaction->amount      = $this->amount;
+        $transaction->reference   = $this->bill->number;
+        $transaction->description = 'PARTIAL REFUND Bill ' . $this->bill->number . ' - ' . $this->bill->customer_name;
+        $transaction->transaction_source = 'refund';
+        $transaction->save();
+        
         //withdrawBillFees
         $transaction = new Transaction;
         $transaction->user_id     = $this->bill->user_id;
@@ -62,16 +73,6 @@ class PartialRefundTransactionsForOwner
         $transaction->amount      = $payment_fees_vat;
         $transaction->reference   = $this->bill->number;
         $transaction->description = 'PARTIAL REFUND VAT';
-        $transaction->transaction_source = 'refund';
-        $transaction->save();
-
-        $transaction = new Transaction;
-        $transaction->user_id     = $this->bill->user_id;
-        $transaction->bill_id     = $this->bill->id;
-        $transaction->type        = 'debit';
-        $transaction->amount      = $this->amount;
-        $transaction->reference   = $this->bill->number;
-        $transaction->description = 'PARTIAL REFUND Bill ' . $this->bill->number . ' - ' . $this->bill->customer_name;
         $transaction->transaction_source = 'refund';
         $transaction->save();
     }
