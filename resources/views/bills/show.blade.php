f57d771dac387467e5f80ac13e3ea032f0581994
abdullah ghanem <aghanem@sure.com.sa>
2021-04-28 11:57:13 +0200

    update Localization @SBV2-380

--- resources/views/bills/show.blade.php
+++ resources/views/bills/show.blade.php
@@ -264,7 +264,7 @@
               <input  type="radio" id="ConfirmRefund" name="refund" class="position-absolute w-100 h-100" value="refund" checked>
               <div class="txt bg-light border text-body p-2 mb-2 d-flex align-items-center justify-content-start">
                 <div class="checkmark rounded-circle position-relative d-flex align-items-center justify-content-center"><p class="rounded-circle bg-white m-0 d-block"></p></div>
-                <span class="d-block">{{__('Confirm Refund Bill')}}</span>
+                <span class="d-block">{{__('Total refund')}}</span>
               </div><!-- txt -->
             </label>
             <label for="PartialRefund" class="position-relative d-block">
