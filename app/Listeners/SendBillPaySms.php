6624d2b652152a1179d526abe0d17111235cbaaa
Amr Aly Abdelaty <Aabdelaty@sure.com.sa>
2021-05-30 18:05:14 +0200

    change sms gateway

--- app/Listeners/SendBillPaySms.php
+++ app/Listeners/SendBillPaySms.php
@@ -39,15 +39,29 @@ class SendBillPaySms implements ShouldQueue
 
         if($event->bill->send_sms && ($event->bill->application_id == null || $event->bill->application->channel_id == null)){
             $message = __('Hello :name, You’ve got a new bill of :total SAR, From :business_name, Pay now :url', [
-                'total' => round($event->bill->total, 2), 
-                'business_name' => $event->bill->user->business_name, 
-                'name' => $event->bill->customer_name, 
+                'total' => round($event->bill->total, 2),
+                'business_name' => $event->bill->user->business_name,
+                'name' => $event->bill->customer_name,
                 'url' => $event->bill->pay_url
             ]);
-            if(app()->environment('production')){
+            if(app()->environment('production'))
+            {
                 $mobile = (int) $event->bill->customer_mobile;
-                $mobile = (int) '966'.$mobile;
-                UnifonicFacade::send($mobile, $message);
+                $data = ["Tagname" => "Sure-Easy", "RecepientNumber" => "0".$mobile, "Message" => $message, "Username" => env('YAMAMAH_USERNAME'), "Password" => env('YAMAMAH_PASSWORD')];
+                $payload = json_encode($data);
+                $ch = curl_init('http://api.yamamah.com/SendSMS');
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+                curl_setopt($ch, CURLOPT_POST, true);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Content-Length: ' . strlen($payload)));
+                $result = curl_exec($ch);
+                curl_close($ch);
+                $response = json_decode($result, true);
+
+                // $mobile = (int) $event->bill->customer_mobile;
+                // $mobile = (int) '966'.$mobile;
+                // UnifonicFacade::send($mobile, $message);
             }
         }
     }
