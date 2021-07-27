bb6ddefa8ba0af138aecf10f5721ea4c97d4da9b
Essam Abdel-Sabour <me@essam.ws>
2021-07-27 15:11:43 +0200

    ga

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,6 +34,15 @@
     #paymentslog {display: none;}
   }
 </style>
+@if (env('APP_ENV') == 'Production')
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K4WN2GW');</script>
+<!-- End Google Tag Manager -->
+@endif
   </head>
   <body id="app-container" class=" @if(app()->getLocale() == 'ar') rtl @else ltr @endif 
     @if(auth()->user()->is_complete_profile) 
@@ -59,6 +68,13 @@
 
     @include('layouts.footer')
     @stack('footer-scripts')
+
+@if (env('APP_ENV') == 'Production')
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K4WN2GW"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+@endif
 
 </body>
 
