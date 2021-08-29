db1e4d2a0499238a75e3fce4e339ec7ee4e97761
abdullah ghanem <aghanem@sure.com.sa>
2021-08-29 15:07:07 +0200

    add translation @SBV2-488

--- nova-components/Settlements/resources/js/components/Create.vue
+++ nova-components/Settlements/resources/js/components/Create.vue
@@ -317,7 +317,7 @@ export default {
                             duration:3,
                             render: h => {
                                 return h('span', [
-                                    this.language == 'en'? 'No bills in selected date range': 'لا يوجد اي فواتير في التاريخ'
+                                    this.language == 'en'? 'No transaction on this date': 'لا يوجد معاملات في هذا التاريخ'
                                 ])
                             }
                         });
