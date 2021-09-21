d42a2f97d980afabc82602753fdac04262dc05cb
Essam Abdel-Sabour <me@essam.ws>
2021-09-21 13:02:10 +0200

    sps end point

--- app/Services/TransferOperations.php
+++ app/Services/TransferOperations.php
@@ -109,7 +109,7 @@ class TransferOperations
     {
         return [
             'referenceNumber' => (string) $transfer->id,
-            'amount' => $transfer->amount,
+            'amount' => $transfer->net_amount,
             'beneficiaryName' => $transfer->user->beneficiary_name,
             'beneficiaryIban' => $transfer->user->iban_number,
             'beneficiaryStreet' => $transfer->user->business_address,
