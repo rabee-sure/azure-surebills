9c9c9a69ad47d02cf1837e50a6a1e434be91e861
abdullah ghanem <aghanem@sure.com.sa>
2021-10-31 15:14:23 +0200

    finish merchants_summary sheet

---
+++ app/Console/Commands/TransfersSummary.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Exports\DueAmountsExport;
+use App\Exports\MerchantsSummaryExport;
+use App\Exports\TransactionsExport;
+use App\Http\Resources\TransactionExportResource;
+use App\Models\Application;
+use App\Models\Transaction;
+use App\Models\Transfer;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+use Maatwebsite\Excel\Facades\Excel;
+
+class TransfersSummary extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'transfers:summary {id?*  : The IDs of the transfers}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'create transfer summary excel sheet.';
+
+
+    protected $CHANNEL_SOURCES = [
+        'channel_extra_amount',
+        'channel_extra_amount_vat',
+        'channel_extra_amount_fees',
+        'channel_fees',
+        'channel_vat',
+    ];
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $ids = (count($this->argument('id')) == 1) ? explode(',', $this->argument('id')[0]):$this->argument('id');
+        $transfers = Transfer::whereIn('id', $ids)->with('user')->get();
+        $this->createMerchantsSummaryFile($transfers);
+        // $this->createDueAmountsFile($transfers->pluck('id')->toArray());
+    }
+
+
+    public function createMerchantsSummaryFile($transfers)
+    {
+        $data = $this->getMerchantsSummaryData($transfers);
+
+        $t_file_n = implode('_', $transfers->pluck('id')->toArray());
+        $file_name = "summary_transfers/$t_file_n/merchants_summary.xlsx";
+
+        Excel::store(new MerchantsSummaryExport($data), $file_name , 'public');
+    } 
+
+
+    public function createDueAmountsFile($transfer_ids)
+    {
+        $transactions = Transaction::whereHas('transfers', function($q) use($transfer_ids){
+                $q->whereIn('transfer_id', $transfer_ids)->whereNotIn('transaction_source', $this->CHANNEL_SOURCES);
+            })->with('bill.application.channel')->get();
+        $rr = [
+            'merchant_id' => '',
+            'merchant_name' => '',
+            'merchan_iban' => '',
+            'bank' => '',
+            'total_amount' => '',
+            'total_fees' => '',
+            'total_fees_vat' => '',
+            'sure_fees' => '',
+            'sure_fees_vat' => '',
+            'channel_fees' => '',
+            'channel_fees_vat' => '',
+            'bank_charges' => '',
+            'net_due' => '',
+        ];
+        $t_file_n = implode('_', $transfer_ids);
+        $file_name = "summary_transfers/$t_file_n/merchants_summary.xlsx";
+        $data = json_decode((TransactionExportResource::collection($transactions))->toJson(), true);
+        Excel::store(new DueAmountsExport($data), $file_name , 'public');
+    } 
+
+
+    public function getMerchantsSummaryData($transfers)
+    {
+        $data = [];
+        $transfer_ids = $transfers->pluck('id')->toArray();
+        $transactions = Transaction::whereHas('transfers', function($q) use($transfer_ids){
+                $q->whereIn('transfer_id', $transfer_ids)->whereNotIn('transaction_source', $this->CHANNEL_SOURCES);
+            })->with('bill.application.channel')->get();
+
+        foreach($transfers as $transfer){
+            $user = $transfer->user;
+            $user_transactions =  $transactions->where('user_id', $user->id);
+            $bills = $transactions->pluck('bill')->unique()->where('user_id', $user->id);
+            $payment_methods = array_count_values($bills->pluck('paymentMethodDetails')->toArray());
+            $data[] = $this->getItem($user, $user_transactions, $bills, 'MADA');
+            $data[] = $this->getItem($user, $user_transactions, $bills, 'VISA');
+            $data[] = $this->getItem($user, $user_transactions, $bills, 'MASTERCARD');
+        }
+
+        return $data;
+    }
+
+    public function getItem($user, $transactions, $bills, $card_brand)
+    {
+        $bills = $bills->where('paymentMethodDetails',$card_brand);
+        $pricing = $bills->whereNotNull('pricing')->first()->pricing ?? [];
+
+        $application_id = $bills->whereNotNull('application_id')->first()->application_id ?? null;
+        $channel_id = $application_id ? Application::find($application_id)->channel_id ?? '' : '';
+
+        return [
+            'client_id' => $user->id,
+            'payment_type' => $card_brand,
+            'no_of_trx' => $bills->count(),
+            'total_amount' => $bills->sum('total'),
+            'total_fees' => $bills->sum('payment_fees'),
+            'total_fees_vat' => $bills->sum('payment_fees_vat'),
+            'total_fees_variable_rate' => $pricing['fees_percentage'] ?? '',
+            'total_fees_fixed_rate' => $pricing['fees_fixed'] ?? '',
+            'sure_variable_rate' => $pricing['surebills_fees_percentage'] ?? '',
+            'sure_fixed_rate' => $pricing['surebills_fees_fixed'] ?? '',
+            'channel_variable_rate' => $pricing['channel_fees_percentage'] ?? '',
+            'channel_fixed_rate' => $pricing['channel_fees_fixed'] ?? '',
+
+
+            'sure_fees' => $bills->sum('payment_surebills_fees'),
+            'sure_vat' => $bills->sum('payment_surebills_fees_vat'),
+            'channel_fees' => $bills->sum('payment_channel_fees'),
+            'channels_vat' => $bills->sum('payment_channel_fees_vat'),
+            'channel_id' => $channel_id,
+        ];
+    }
+}
