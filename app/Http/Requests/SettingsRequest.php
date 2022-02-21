e46a25823524511858cc21ee53f8e250cdc4f212
mzain2022 <mzain@sure.com.sa>
2022-02-21 14:46:15 +0200

    make additional number not requeired

--- app/Http/Requests/SettingsRequest.php
+++ app/Http/Requests/SettingsRequest.php
@@ -41,7 +41,6 @@ class SettingsRequest extends FormRequest
             $rules['street_name'] = ['required_if:add_tax_invoice,on'];
             $rules['district'] = ['required_if:add_tax_invoice,on'];
             $rules['postal_code'] = ['required_if:add_tax_invoice,on'];
-            $rules['additional_no'] = ['required_if:add_tax_invoice,on'];
         }
 
         return $rules;
@@ -118,7 +117,6 @@ class SettingsRequest extends FormRequest
           'street_name.required_if' => __('The street name field is required when add tax invoice is on.'),
           'district.required_if' => __('The district field is required when add tax invoice is on.'),
           'postal_code.required_if' => __('The postal code field is required when add tax invoice is on.'),
-          'additional_no.required_if' => __('The additional no field is required when add tax invoice is on.'),
         ];
     }
 }
