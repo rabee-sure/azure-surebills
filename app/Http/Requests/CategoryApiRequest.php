f84020b77dd738f925361a7c51e2e02b875e63e7
mzain2022 <mzain@sure.com.sa>
2022-02-27 15:26:15 +0200

    cartegory crud

---
+++ app/Http/Requests/CategoryApiRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Rules\ValidateUploadFile;
+
+class CategoryApiRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name_en' => ['required'],
+            'name_ar' => ['required'],
+            'image' => ['required', new ValidateUploadFile(['png', 'jpg', 'jpeg'])],
+            'sort_number' => ['required'],
+            'active' => ['required'],
+        ];
+    }
+}
