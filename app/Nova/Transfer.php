c31bea99e90631b0002374462d0cd47e7dca04fa
Amr Aly Abdelaty <amraly_267@yahoo.com>
2022-03-22 12:05:11 +0200

    add ID column and business name to transfer

--- app/Nova/Transfer.php
+++ app/Nova/Transfer.php
@@ -106,9 +106,7 @@ class Transfer extends Resource
         return [
             ID::make()->sortable(),
 
-            BelongsTo::make(__('User ID'), 'user', User::class)->displayUsing(function($user){
-                return $user->id;
-            })->searchable(),
+            Text::make(__('User ID'), 'user_id'),
 
             BelongsTo::make(__('Business Name'), 'user', User::class)->displayUsing(function($user){
                 return $user->business_name_en ? $user->business_name_en : $user->mainStoreUser->business_name_en;
