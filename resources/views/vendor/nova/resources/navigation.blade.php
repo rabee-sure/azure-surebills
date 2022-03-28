8b30257c4b77f54a31af037f5c1f1fe74e12553e
Amr Aly Abdelaty <amraly_267@yahoo.com>
2022-03-28 17:12:34 +0200

    create side menu Reports category

--- resources/views/vendor/nova/resources/navigation.blade.php
+++ resources/views/vendor/nova/resources/navigation.blade.php
@@ -1,5 +1,30 @@
 @if (count(Nova::availableResources(request())))
 <ul class="sidemenu">
+
+    <li class="sidebar-dropdown mb-2">
+        <input type="checkbox" checked>
+        <a href="#" data-toggle="dropdown">
+        <span class="sidebar-label ml-8">{{ __('Reports') }} </span></a>
+        <ul class="dropdown-menu">
+            <li class="sidebar-dropdown">
+                <router-link :to="{name: 'index', params: {resourceName: 'users'}}" class="flex items-center font-normal text-white mb-6 text-base no-underline dim">
+                    <svg class="sidebar-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path fill="var(--sidebar-icon)" d="M3 1h4c1.1045695 0 2 .8954305 2 2v4c0 1.1045695-.8954305 2-2 2H3c-1.1045695 0-2-.8954305-2-2V3c0-1.1045695.8954305-2 2-2zm0 2v4h4V3H3zm10-2h4c1.1045695 0 2 .8954305 2 2v4c0 1.1045695-.8954305 2-2 2h-4c-1.1045695 0-2-.8954305-2-2V3c0-1.1045695.8954305-2 2-2zm0 2v4h4V3h-4zM3 11h4c1.1045695 0 2 .8954305 2 2v4c0 1.1045695-.8954305 2-2 2H3c-1.1045695 0-2-.8954305-2-2v-4c0-1.1045695.8954305-2 2-2zm0 2v4h4v-4H3zm10-2h4c1.1045695 0 2 .8954305 2 2v4c0 1.1045695-.8954305 2-2 2h-4c-1.1045695 0-2-.8954305-2-2v-4c0-1.1045695.8954305-2 2-2zm0 2v4h4v-4h-4z"/>
+                    </svg>
+                    <span class="sidebar-label">{{ __('Merchants Outstanding') }} </span>
+                </router-link>
+            </li>
+            <li class="sidebar-dropdown">
+                <router-link :to="{name: 'index', params: {resourceName: 'auto-transfers'}}" class="flex items-center font-normal text-white mb-6 text-base no-underline dim">
+                    <svg class="sidebar-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path fill="var(--sidebar-icon)" d="M3 1h4c1.1045695 0 2 .8954305 2 2v4c0 1.1045695-.8954305 2-2 2H3c-1.1045695 0-2-.8954305-2-2V3c0-1.1045695.8954305-2 2-2zm0 2v4h4V3H3zm10-2h4c1.1045695 0 2 .8954305 2 2v4c0 1.1045695-.8954305 2-2 2h-4c-1.1045695 0-2-.8954305-2-2V3c0-1.1045695.8954305-2 2-2zm0 2v4h4V3h-4zM3 11h4c1.1045695 0 2 .8954305 2 2v4c0 1.1045695-.8954305 2-2 2H3c-1.1045695 0-2-.8954305-2-2v-4c0-1.1045695.8954305-2 2-2zm0 2v4h4v-4H3zm10-2h4c1.1045695 0 2 .8954305 2 2v4c0 1.1045695-.8954305 2-2 2h-4c-1.1045695 0-2-.8954305-2-2v-4c0-1.1045695.8954305-2 2-2zm0 2v4h4v-4h-4z"/>
+                    </svg>
+                    <span class="sidebar-label">{{ __('AutoTransfer') }} </span>
+                </router-link>
+            </li>
+        </ul>
+    </li>
+
     <li class="sidebar-dropdown">
         <router-link :to="{
             name: 'index',
