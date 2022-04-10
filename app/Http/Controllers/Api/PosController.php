0980aac3d3eca7423c0a1a986ef665fd2f3ed089
mzain2022 <mzain@sure.com.sa>
2022-04-10 17:43:10 +0200

    created bill directe without listener and return pay url in response

--- app/Http/Controllers/Api/PosController.php
+++ app/Http/Controllers/Api/PosController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\CategoryResource;
 use App\Http\Resources\SubCategoryResource;
 use App\Http\Resources\CategoryPosListResource;
-use App\Http\Resources\PosOrderApiResource;
+use App\Http\Resources\BillApiResource;
 
 use App\Models\Category;
 use App\Http\Resources\ProductResource;
@@ -15,12 +15,17 @@ use App\Models\Customer;
 use App\Models\ProductImage;
 use App\Models\PosOrder;
 use App\Models\PosOrderItem;
+use App\Models\Bill;
+use App\Models\BillItem;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException as ValidationsException;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\CustomerApiRequest;
 use App\Http\Requests\PosOrderApiRequest;
 
-use App\Events\OrderCreated;
+use App\Events\BillCreated;
 
 class PosController extends Controller
 {
@@ -188,8 +193,88 @@ class PosController extends Controller
         $order->total = $sub_total - $discount + $vat;
         $order->save();
 
-        event(new OrderCreated($order));
+        
+        $bill = DB::transaction(function () use ($order) {
+            $user = User::find($order->user_id);
 
-        return new PosOrderApiResource($order);
+            $bill = Bill::create([
+                'user_id' => $user->store_main_user_id ?? $user->id,
+                'status' => 'pending',
+                'business_name' => $order->business_name,
+                'customer_id' => $order->customer_id,
+                'customer_name' => $order->customer_name,
+                'customer_email' => $order->customer_email,
+                'customer_mobile' => $order->customer_mobile,
+                'customer_notes' => $order->customer_notes,
+
+                'reference_id' => $order->number,
+
+                'expiry_date' => 30,
+                'expiry_hours' => $request->expiry_hours ?? 0,
+                'expiry_minutes' => $request->expiry_minutes ?? 0,
+                'due_date' => date('Y-m-d', strtotime(str_replace('/', '-', $order->created_at))),
+
+                'add_discount' => $order->add_discount  ? "on" : 0,
+                'discount_type' => $order->add_discount  ? $order->discount_type : false,
+                'discount_value' => $order->add_discount  ? $order->discount_value : null,
+
+                'add_tax' => $order->add_tax ? "on" : false,
+                'tax_name' => $order->add_tax ? $order->tax_name : null,
+                'tax_value' => $order->add_tax ? $order->tax_value : null,
+
+                'send_sms' => false,
+                'send_email' => false,
+            ]);
+
+            foreach ($order->items as $item) {
+                BillItem::create([
+                    'bill_id' => $bill->id,
+                    'product_name' => $item->product_name,
+                    'product_price' => $item->product_price,
+                    'quantity' => $item->quantity,
+                    'total' => $item->quantity * $item->product_price,
+                ]);
+            }
+
+            $sub_total = $bill->items->sum('total');
+            $discount = 0;
+            $vat = 0;
+            $payment_fees = 0;
+
+            if ($user->pay_fees == "client") {
+                $payment_fees = ($sub_total * ($user->credit_cards_percentage / 100)) + $user->credit_cards_fixed;
+            }
+
+            if ($order->add_discount) {
+                switch ($order->discount_type) {
+                    case 'fixed':
+                        $discount = $order->discount_value;
+                        break;
+                    case 'percentage':
+                        $discount = ($sub_total + $payment_fees) * $order->discount_value / 100;
+                        break;
+                }
+            }
+
+            if ($order->add_tax) {
+                $vat = ($sub_total + $payment_fees - $discount) * $order->tax_value / 100;
+            }
+
+            $bill->payment_fees = $payment_fees;
+            $bill->discount = $discount;
+            $bill->vat = $vat;
+            $bill->number = $bill->getNumber();
+            $bill->sub_total = $sub_total;
+            $bill->total = $sub_total + $payment_fees - $discount + $vat;
+            if ($bill->total <= 0) {
+                throw ValidationsException::withMessages(['total' => __('The total must be greater than 0')]);
+            }
+            $bill->save();
+            return $bill;
+        });
+
+        event(new BillCreated($bill));
+
+        return new BillApiResource($bill);
     }
 }
