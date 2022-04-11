bddad869e318a6e169417e728e9bf0dc592e6c8f
Amr Aly Abdelaty <amraly_267@yahoo.com>
2022-04-11 14:50:02 +0200

    bill report

---
+++ app/Nova/BillReport.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Kpolicar\DateRange\DateRange;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class BillReport extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = \App\Models\Report::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make(__('ID'), 'id')->sortable(),
+
+            Text::make(__('Paid Period'), function(){
+                return __('From :from To :to', ['from' => $this->parameters["from"], 'to' => $this->parameters["to"]]);
+            })->exceptOnForms(),
+
+            DateRange::make(__('Paid Period'), ['from', 'to'])->onlyOnForms(),
+
+        ];
+    }
+
+    public static function indexQuery(NovaRequest $request, $query)
+    {
+        return $query->where('frontpage', true);
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}
