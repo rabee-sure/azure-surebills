a00ab16ac8ffefb1df7ea1ad4670f8ff3f0f5e85
mzain2022 <mzain@sure.com.sa>
2022-05-17 15:42:00 +0200

    generate excel report for bills customer data

---
+++ app/Exports/BillCustomerDataExport.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Exports;
+
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Illuminate\Support\Facades\DB;
+use Maatwebsite\Excel\Concerns\Exportable;
+
+class BillCustomerDataExport implements FromQuery, WithHeadings
+{
+    use Exportable;
+
+    public function headings(): array
+    {
+        return [
+            'Bill Number',
+            'Bill Status',
+            'Bill Created at',
+            'Payment Method',
+            'Customer Name',
+            'Customer Mobile',
+            'Customer Email',
+        ];
+    }
+
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function query()
+    {
+        $query = DB::table('bills')
+        ->select('number', 'status', 'created_at', 'payment_method', 'customer_name', 'customer_mobile', 'customer_email')
+        ->where('user_id', 1654)
+        ->whereIn('status', ['paid','paid_cash','paid_bank_transfer'])
+        ->orderByDesc('created_at');
+        return $query;
+    }
+}
