b15071af0c8bbfebfb67faaf9e480c9db98945d0
Essam AbdelSabour <me@essam.ws>
2022-06-14 12:15:57 +0200

    trace issue

--- app/Http/Controllers/StatementController.php
+++ app/Http/Controllers/StatementController.php
@@ -33,24 +33,21 @@ class StatementController extends Controller
         $application = ($request->has('application_id') && !in_array($request->application_id, ['all','undefined']))? Application::find($request->application_id) : null;
 
         $statementQuery = auth()->user()->getStatement();
-        $statementQueryAllCredit = clone $statementQuery;
-        $statementQueryAllDebit = clone $statementQuery;
+//        $statementQueryAllCredit = clone $statementQuery;
+//        $statementQueryAllDebit = clone $statementQuery;
 
         $statement = $statementQuery->paginate(100);
         $channels = Channel::userId(auth()->user()->store_main_user_id ?? auth()->user()->id)->get();
         $applications = ($channel) ? $channel->applications : [];
 
         $totals = [];
-        $credit = $statementQueryAllCredit->where('type', 'credit')->selectRaw("SUM(amount) AS credit");
-        $debit = $statementQueryAllDebit->where('type', 'debit')->selectRaw("SUM(amount) AS debit");
+//        $credit = $statementQueryAllCredit->where('type', 'credit')->selectRaw("SUM(amount) AS credit");
+//        $debit = $statementQueryAllDebit->where('type', 'debit')->selectRaw("SUM(amount) AS debit");
 
-        $totals['credit'] = ($credit->first() != null) ? round2($credit->first()->credit) : 0;
-        $totals['debit'] = ($debit->first() != null) ? round2($debit->first()->debit) : 0;
+        $totals['credit'] = 0;//($credit->first() != null) ? round2($credit->first()->credit) : 0;
+        $totals['debit'] = 0;//($debit->first() != null) ? round2($debit->first()->debit) : 0;
 
-        $totals['all'] = round2($totals['credit'] - $totals['debit']);
-
-
-        return 'a';
+        $totals['all'] = 0;//round2($totals['credit'] - $totals['debit']);
 
         return view('statements.index', compact('statement', 'date_start', 'date_to', 'channels', 'channel', 'applications', 'application', 'totals'));
     }
