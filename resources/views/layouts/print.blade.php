be7f9834eb4b28b37958ac76bf4d441df38e407b
Ahmed Rabee <dev.ahmedrabee@gmail.com>
2022-07-18 13:26:32 +0200

    fix style print pages

--- resources/views/layouts/print.blade.php
+++ resources/views/layouts/print.blade.php
@@ -8,7 +8,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Almarai:wght@300;400;700&family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/new/css/main.css?v={{ config('app.asset_version') }}" />
+    <link rel="stylesheet" href="/new/css/print.css?v={{ config('app.asset_version') }}" />
   </head>
   <body>
     @yield('content')
