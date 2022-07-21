0db5861ec37331e71eed8014b0c815e9fa26f03e
mzain2022 <mzain@sure.com.sa>
2022-07-21 16:39:01 +0200

    add new fields to merchant object and log reponse

--- app/Listeners/SPSSendMerchantData.php
+++ app/Listeners/SPSSendMerchantData.php
@@ -3,9 +3,11 @@
 namespace App\Listeners;
 
 use Illuminate\Auth\Events\Registered;
+use App\Events\UserUpdated;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Log;
 
 class SPSSendMerchantData
 {
@@ -22,16 +24,17 @@ class SPSSendMerchantData
     /**
      * Handle the event.
      *
-     * @param  \App\Events\Registered  $event
+     * @param  \App\Events\UserUpdated  $event
      * @return void
      */
-    public function handle(Registered $event)
+    public function handle(UserUpdated $event)
     {
         //Get merchant account data
         $user = $event->user;
 
         //prepare data of object
         $data = array();
+        $data['userId'] = $user->id;
         $data['MerchantNameAr'] = $user->name;
         $data['MerchantNameEn'] = $user->name;
         $data['CommercialNameAr'] = $user->business_name_ar;
@@ -44,14 +47,19 @@ class SPSSendMerchantData
         $data['BeneficiaryIban'] = $user->iban_number;
         $data['BeneficiaryCity'] = $user->city;
         $data['BeneficiaryStreet'] = $user->street_name;
+        $data['ExternalMerchantId'] = $user->id;
+        $data['Verified'] = $user->verified;
 
         //Send merchant account data to sps api
         $link = config('sps.base_url').'/'.config('sps.routes.save_merchants');
         $client = new Client;
-        $response = $client->request('POST', $link, ['body' => json_encode($data)]);
+        $response = $client->request('POST', $link, [
+            'body' => json_encode($data),
+            // 'curl' => [CURLOPT_SSLVERSION => CURL_SSLVERSION_SSLv3]
+        ]);
 
         //Log Api faild response
-
+        log::info('SPS response', json_decode($response->getBody()));
         return $response;
     }
 }
