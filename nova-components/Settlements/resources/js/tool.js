08d53014608749ea073fcd931d0776f03491a7b6
mzain2022 <mzain@sure.com.sa>
2022-08-08 14:18:17 +0200

    miising files with dev merge

--- nova-components/Settlements/resources/js/tool.js
+++ nova-components/Settlements/resources/js/tool.js
@@ -7,15 +7,8 @@ import 'sweetalert2/dist/sweetalert2.min.css';
 
 import JsonExcel from "vue-json-excel";
 
-<<<<<<< HEAD
-import VueSweetalert2 from 'vue-sweetalert2';
-// If you don't need the styles, do not connect
-import 'sweetalert2/dist/sweetalert2.min.css';
 
-
-=======
 // const Swal = require('sweetalert2')
->>>>>>> 0143250c2f25fb65f8c2dd91910b937f8b99c72c
 Nova.booting((Vue, router, store) => {
   Vue.component("downloadExcel", JsonExcel);
   Vue.use(ViewUI, { locale });
