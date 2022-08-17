fc6d19d4ed47db2108670f4270a1f7401544a24e
Amr Aly Abdelaty <amraly_267@yahoo.com>
2022-08-17 17:45:14 +0200

    fix create permissions to super admin user issue

--- database/seeds/CreateSuperAdminUserSeeder.php
+++ database/seeds/CreateSuperAdminUserSeeder.php
@@ -20,10 +20,11 @@ class CreateSuperAdminUserSeeder extends Seeder
         $users = User::whereNull('store_main_user_id')->get();
         $role = Role::firstOrCreate(['name' => 'super admin', 'guard_name' => 'web'], ['name' => 'super admin', 'guard_name' => 'web']);
 
+        $permissions = Permission::where('guard_name', 'web')->pluck('id')->all();
+        $role->syncPermissions($permissions);
+
         foreach($users as $user)
         {
-            $permissions = Permission::where('guard_name', 'web')->pluck('id')->all();
-            $role->syncPermissions($permissions);
             $user->assignRole($role->id);
         }
     }
