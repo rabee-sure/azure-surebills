89d4ec8825a36117108719a5da806a87d2b0d8ca
mzain2022 <mzain@sure.com.sa>
2022-09-14 16:38:22 +0200

    remove json encode form request

--- app/Services/TransferOperations.php
+++ app/Services/TransferOperations.php
@@ -121,7 +121,7 @@ class TransferOperations
 
         try {
             //code...
-            $response = $client->request('POST', $url, ['verify' => false,'body'=>json_encode($body)]);
+            $response = $client->request('POST', $url, ['verify' => false,'body'=>$body]);
             //log $reponse
             \Log::channel('send_to_sps')->info("SPS response", $response->getBody()->getContents());                                            
         } catch (\Throwable $th) {
