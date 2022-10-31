2052784a373bf84d523b49cf14bbb95eb149730b
mzain2022 <mzain@sure.com.sa>
2022-10-31 11:21:46 +0200

    Chenge bill notes to notes

--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -25,8 +25,8 @@
           <input  name="email" type="email" inputmode="email" class="form-control shadow-none bg-white border w-100 rounded-3 text-body" id="Email" placeholder="{{__('Email')}}">
         </div>
         <div class="form-group mb-3">
-          <label for="Notes" class="d-block mb-1">{{__('Notes')}}</label>
-          <input name="notes" type="text" class="form-control shadow-none bg-white border w-100 rounded-3 text-body" id="Notes" placeholder="{{__('Notes')}}">
+          <label for="Notes" class="d-block mb-1">{{__('Customer Notes')}}</label>
+          <input name="notes" type="text" class="form-control shadow-none bg-white border w-100 rounded-3 text-body" id="Notes" placeholder="{{__('Customer Notes')}}">
         </div>
         @if($user->settings->add_tax_invoice)
           <div class="form-group mb-3">
