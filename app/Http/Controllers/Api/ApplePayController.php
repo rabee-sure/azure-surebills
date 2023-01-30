810ea64122eaa6efc05026a6d6a6146b11d74d5a
Essam AbdelSabour <me@essam.ws>
2023-01-30 22:10:01 +0200

    tracking

--- app/Http/Controllers/Api/ApplePayController.php
+++ app/Http/Controllers/Api/ApplePayController.php
@@ -17,8 +17,8 @@ class ApplePayController extends Controller
         $ch = curl_init();
         $data = '{"merchantIdentifier": "'.config('payment.drivers.mastercard_applepay.applepay_merchant_id').'", "domainName":"'.config('payment.drivers.mastercard_applepay.domain').'", "displayName":"SureBills"}';
         curl_setopt($ch, CURLOPT_URL, $request->validationURL);
-        curl_setopt($ch, CURLOPT_SSLCERT, base_path('app/Payment/Drivers/MasterCardApplePay/key.pem'));
-        curl_setopt($ch, CURLOPT_SSLKEY, base_path('app/Payment/Drivers/MasterCardApplePay/key2.pem'));
+        curl_setopt($ch, CURLOPT_SSLCERT, base_path('app/Payment/Drivers/MasterCardApplePay/apple_pay.pem'));
+        curl_setopt($ch, CURLOPT_SSLKEY, base_path('app/Payment/Drivers/MasterCardApplePay/applepay.key.pem'));
 //        curl_setopt($ch, CURLOPT_SSLKEYPASSWD, config('payment.drivers.mastercard_applepay.passwd'));
         //curl_setopt($ch, CURLOPT_PROTOCOLS, CURLPROTO_HTTPS);
         //curl_setopt($ch, CURLOPT_SSLVERSION, 'CURL_SSLVERSION_TLSv1_2');
