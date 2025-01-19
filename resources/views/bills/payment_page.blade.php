9ac2b938e66ffdf73d81cad56bf27e76991f58e2
Amr Aly Abd Elaty <aabdelaty@surepay.sa>
2025-01-19 16:46:33 +0200

    remove apple pay section

--- resources/views/bills/payment_page.blade.php
+++ resources/views/bills/payment_page.blade.php
@@ -59,7 +59,7 @@
                         </div>
                     @endif
                     <div id="payment_area">
-                        @if (!isset($sureEasyRendrer) && $sureEasyRendrer)  
+                        @if (!isset($sureEasyRendrer))  
                         <div class="pay_button border-right border-left bg-light p-2 border-top" id="applepay_button">
                             <button id="payment" class="d-block mx-auto" lang="<?php echo App::getLocale() ?>" style="-webkit-appearance: -apple-pay-button; -apple-pay-button-type: buy; width: 230px; height: 40px; cursor: pointer; border-radius: 5px;"></button>
                         </div><!-- pay_button -->
@@ -158,7 +158,7 @@
         <?php require app_path('Payment/Drivers/MasterCardHostedSession/pay.js'); ?>
 
         {{-- APPLE PAY VIA MASTERCARD --}}
-        @if (!isset($sureEasyRendrer) && $sureEasyRendrer == true)
+        @if (!isset($sureEasyRendrer))
         <?php require app_path('Payment/Drivers/MasterCardApplePay/payment-request.js'); ?>
         @endif
         {{-- APPLE PAY VIA MASTERCARD --}}
