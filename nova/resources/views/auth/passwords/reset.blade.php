1c4ff90a91686a289e995172488b5d0674cc455c
AhmedRabee <akotob@surepay.sa>
2025-03-09 13:29:34 +0200

    fix ui for nova auth pahes

--- nova/resources/views/auth/passwords/reset.blade.php
+++ nova/resources/views/auth/passwords/reset.blade.php
@@ -5,7 +5,7 @@
 @include('nova::auth.partials.header')
 
 <form
-    class="bg-white shadow rounded-lg p-8 max-w-login mx-auto"
+    class="bg-white shadow rounded-lg p-8 max-w-login mx-auto nova-login-area"
     method="POST"
     action="{{ route('nova.password.request') }}"
 >
@@ -19,22 +19,22 @@
 
     <input type="hidden" name="token" value="{{ $token }}">
 
-    <div class="mb-6 {{ $errors->has('email') ? ' has-error' : '' }}">
+    <div class="form-group mb-6 {{ $errors->has('email') ? ' has-error' : '' }}">
         <label class="block font-bold mb-2" for="email">{{ __('Email Address') }}</label>
         <input class="form-control form-input form-input-bordered w-full" id="email" type="email" name="email" value="{{ $email ?? old('email') }}" required autofocus>
     </div>
 
-    <div class="mb-6 {{ $errors->has('password') ? ' has-error' : '' }}">
+    <div class="form-group mb-6 {{ $errors->has('password') ? ' has-error' : '' }}">
         <label class="block font-bold mb-2" for="password">{{ __('Password') }}</label>
         <input class="form-control form-input form-input-bordered w-full" id="password" type="password" name="password" required>
     </div>
 
-    <div class="mb-6 {{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+    <div class="form-group mb-6 {{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
         <label class="block font-bold mb-2" for="password-confirm">{{ __('Confirm Password') }}</label>
         <input class="form-control form-input form-input-bordered w-full" id="password-confirm" type="password" name="password_confirmation" required>
     </div>
 
-    <button class="w-full btn btn-default btn-primary hover:bg-primary-dark" type="submit">
+    <button type="submit">
         {{ __('Reset Password') }}
     </button>
 </form>
