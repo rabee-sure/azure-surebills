8797417a352689509b49c36b1d12a83b980377d8
Amr Aly Abd Elaty <aabdelaty@surepay.sa>
2025-03-27 15:23:28 +0200

    snb payer auth amr

--- app/Services/CyberSourceService.php
+++ app/Services/CyberSourceService.php
@@ -172,10 +172,11 @@ class CyberSourceService extends PaymentAbstract
                     ])
                 ]),
                 'consumerAuthenticationInformation' => new Riskv1decisionsConsumerAuthenticationInformation([
-                    'acsWindowSize' => '02',
+                    'acsWindowSize' => '05',
                     'referenceId' => $payerSetupRefranceId,
                     'transactionMode' => 'S',
-                    'returnUrl' => 'https://wv730hw7033250:3002/restapi/cardinalDirect/StepUp/Response'
+                    'returnUrl' => route('validate-auth-result')
+                    // 'https://wv730hw7033250:3002/restapi/cardinalDirect/StepUp/Response'
                 ])
             ]
         ); // \CyberSource\Model\CheckPayerAuthEnrollmentRequest |
@@ -184,6 +185,7 @@ class CyberSourceService extends PaymentAbstract
             $result = $api_instance->checkPayerAuthEnrollment($checkPayerAuthEnrollmentRequest);
             $resultModel = new RiskV1AuthenticationsPost201Response(json_decode($result[0], true));
             $responseBody = json_decode($resultModel, true);
+            session()->put('transaction_id', $responseBody['consumerAuthenticationInformation']['authenticationTransactionId']);
             return $responseBody;
         } catch (Exception $e) {
             echo 'Exception when calling PayerAuthenticationApi->checkPayerAuthEnrollment: ', $e->getMessage(), PHP_EOL;
--- resources/views/cyber/iframe-2.blade.php
+++ resources/views/cyber/iframe-2.blade.php
@@ -8,8 +8,8 @@
 </head>
 
 <body>
-    <iframe name="step-up-iframe" width="auto" height="auto"></iframe>
-    <form id="step-up-form" target=”step-up-iframe" method="post"
+    <iframe name="step-up-iframe" width="auto" height="1250"></iframe>
+    <form id="step-up-form" target="step-up-iframe" method="post"
         action="https://centinelapistag.cardinalcommerce.com/V2/Cruise/StepUp"> <input type="hidden" name="JWT"
             value="{{ $token }}" />
         <input type="hidden" name="MD" value="optionally_include_custom_data_that_will_be_returned_as_is" />
