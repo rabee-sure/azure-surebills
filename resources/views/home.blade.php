a432ef583b31beb0f325edfcd7d06d6c435d5079
mzain2022 <mzain@sure.com.sa>
2025-06-30 11:53:45 +0300

    Update bill creation condition in home view to handle null mainStoreUser case

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -150,7 +150,7 @@
     @endcan
 
     @can('create bills')
-      @if(count(auth()->user()->channels) == 0 || (auth()->user()->mainStoreUser && count(auth()->user()->mainStoreUser->channels) == 0))
+      @if((auth()->user()->mainStoreUser == null && count(auth()->user()->channels) == 0) || (auth()->user()->mainStoreUser && count(auth()->user()->mainStoreUser->channels) == 0))
         <a href="{{ route('bills.create')}}" data-bs-toggle="tooltip" data-bs-placement="top" title="{{ __('Create a bill')}}" class="addNewBillBtn position-fixed rounded-circle d-block shadow"></a>
       @endif
     @endcan
