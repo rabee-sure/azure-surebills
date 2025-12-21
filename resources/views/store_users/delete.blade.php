03c2f10822a074a024bcdcba5782d1178d78be51
AhmedRabee <akotob@surepay.sa>
2025-12-21 13:18:24 +0200

    Update user management modals to include a form class for improved styling

--- resources/views/store_users/delete.blade.php
+++ resources/views/store_users/delete.blade.php
@@ -10,7 +10,7 @@
         <div class="closeBtn d-flex align-items-center justify-content-end mb-3 w-100">
           <button type="button" class="d-flex align-items-center justify-content-center border-0 bg-transparent p-0 text-body fs-4" data-bs-dismiss="modal" aria-label="Close"><i class="fal fa-times-circle"></i></button>
         </div><!-- closeBtn -->
-        <form action="{{ route('users.destroy', $user->id)}}" method="post">
+        <form action="{{ route('users.destroy', $user->id)}}" method="post" class="form w-100">
           @csrf
           @method('DELETE')
           <span class="d-block text-center text-body mb-4 fs-5 text-break text-wrap">{{ __('Are You sure Suspend this User?')}}</span>
