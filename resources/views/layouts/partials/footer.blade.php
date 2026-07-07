2518bdace8f0755faaef6559ebb9b64990f86721
AhmedRabee <akotob@surepay.sa>
2026-07-07 14:58:46 +0300

    Update footer localization and enhance footer display
    - Added localized footer text in Arabic and English language files for better user experience.
    - Refactored footer display in the Blade template to utilize the new localization keys, improving maintainability and consistency across languages.

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -2,12 +2,10 @@
   <div class="container-fluid">
     <div class="footer-container d-flex align-items-center justify-content-between py-4 flex-md-row flex-column">
       <div class="text-body">
-        صُنع بـ ❤️ بواسطة <a href="https://surepay.sa" target="_blank" class="footer-link">SurePay</a> © {{ date('Y') }}
+        {{ trans('footer_text') }} ❤️ {{ trans('footer_text_2') }} <a href="https://surepay.sa" target="_blank" class="footer-link">SurePay</a> © {{ date('Y') }}
       </div>
       <div class="d-none d-lg-inline-block">
-        <a href="{{ url('/') }}/privacy" class="footer-link me-4" target="_blank" title="{{trans('privacy_policy')}}"
-          >{{trans('privacy_policy')}}</a
-        >
+        <a href="{{ url('/') }}/privacy" class="footer-link me-4" target="_blank" title="{{ trans('privacy_policy') }}">{{ trans('privacy_policy') }}</a>
       </div>
     </div>
   </div>
