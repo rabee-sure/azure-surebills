2ac4f55ea71e5b68bbd112bef1208f1e8a964b6c
mzain2022 <mzain@sure.com.sa>
2026-08-13 18:07:11 +0300

    Add file upload UI enhancements and localization support
    - Updated file input in business_information.blade.php and step2.blade.php to include a custom upload area with a "No file chosen..." placeholder.
    - Added corresponding translations for the new placeholder in both Arabic and English language files (ar.json and en.json).
    - Improved user experience by providing clearer feedback on file selection.
    
    https://surepayproducts.atlassian.net/browse/SBV2-3267

--- resources/views/account/business_information.blade.php
+++ resources/views/account/business_information.blade.php
@@ -93,7 +93,13 @@
         @endif
         <div class="col">
           <label for="logo" class="form-label">{{ __('Logo') }}</label>
-          <input name="logo" type="file" id="logo" class="form-control" accept="image/png, image/jpeg, image/jpg">
+          <div class="uploadFiledArea">
+            <div class="uploadInput">
+              <div class="fileName">{{ __('No file chosen...') }}</div>
+              <div class="fileBtn">{{ __('Choose file') }}</div>
+            </div><!-- uploadInput -->
+            <input name="logo" type="file" id="logo" accept="image/png, image/jpeg, image/jpg">
+          </div><!-- uploadFiledArea -->
           <input type="hidden" name="hidden_logo" value="{{ auth()->user()->logo }}" />
           @if($errors->has('logo'))
             <span id="inputEmail8-error" class="invalid-feedback" style="display: inline;">{{ $errors->first('logo') }}</span>
@@ -254,7 +260,7 @@
     $('#logo').bind('change', function () {
       var filename = $("#logo").val();
       if (/^\s*$/.test(filename)) {
-        $(".fileName").text("No file chosen...");
+        $(".fileName").text("{{ __('No file chosen...') }}");
       }
       else {
         $(".fileName").text(filename.replace("C:\\fakepath\\", ""));
