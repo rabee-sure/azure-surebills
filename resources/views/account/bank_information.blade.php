4071a05ef70d4863ccb2f7ccad407d7e903e156e
AhmedRabee <akotob@surepay.sa>
2026-08-20 11:41:44 +0300

    SBV2-3321 [Merchant/Settings/Bank Information]Lowercase Labels on Bank Information Screen

--- resources/views/account/bank_information.blade.php
+++ resources/views/account/bank_information.blade.php
@@ -25,7 +25,7 @@
     <div class="card-body">
       <div class="row g-6">
         <div class="col-12 col-md-6">
-          <label for="bank_id" class="form-label">{{__('Bank')}} <span class="text-danger">*</span></label>
+          <label for="bank_id" class="form-label text-capitalize">{{__('Bank')}} <span class="text-danger">*</span></label>
           <select name="bank_id" id="bank_id" class="form-select select2" @if($user->disable_bank_documents) disabled @endif>
             <option value="" disabled selected>{{__('Select your Bank')}}</option>
             @foreach(App\Models\Bank::active()->get() as $bank)
@@ -34,19 +34,19 @@
           </select>
         </div><!-- col-12 -->
         <div class="col-12 col-md-6">
-          <label for="iban_number" class="form-label">{{__('IBAN Number')}} <span class="text-danger">*</span></label>
+          <label for="iban_number" class="form-label text-capitalize">{{__('IBAN Number')}} <span class="text-danger">*</span></label>
           <input value="{{ $user->iban_number }}"  name="iban_number" type="text" class="form-control" id="iban_number" placeholder="رقم آيبان مثلاً : SA2720000000000000001212 *" aria-describedby="ibanNumberHelp" autocomplete="off" @if($user->disable_bank_documents) disabled @endif>
           <div id="ibanNumberHelp" class="form-text">{{__('This account will be used to settle payments received through point-of-sale devices')}}</div>
         </div><!-- col-12 -->
         <div class="col-12">
-          <label for="beneficiary_name" class="form-label">{{__('Beneficiary Name')}} <span class="text-danger">*</span></label>
+          <label for="beneficiary_name" class="form-label text-capitalize">{{__('Beneficiary Name')}} <span class="text-danger">*</span></label>
           <input value="{{ $user->beneficiary_name }}" name="beneficiary_name" type="text" class="form-control" id="beneficiary_name" placeholder="{{__('Beneficiary Name')}}" aria-describedby="beneficiaryNameHelp" autocomplete="off" @if($user->disable_bank_documents) disabled @endif>
           <div id="beneficiaryNameHelp" class="form-text">{{__('Write the name of the account holder in English as registered with the bank')}}</div>
         </div><!-- col-12 -->
         <div class="col-12">
           <hr class="mb-6 mt-0" />
           <label for="commercial_registry_expiry_date" class="form-label d-flex align-items-start justify-content-start flex-column">
-            <span class="d-block fs-5 mb-1">{{ __('Upload the required documents') }}</span>
+            <span class="d-block fs-5 mb-1 text-capitalize">{{ __('Upload the required documents') }}</span>
             <span class="text-muted mb-2">{{ __('Upload a copy of the IBAN card or an account statement showing the IBAN number and the name of the facility') }}</span>
           </label>
           @include('components.dropzone',[
