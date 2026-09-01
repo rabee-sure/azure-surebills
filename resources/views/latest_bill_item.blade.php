cdc9634625ede1496e3c3787e2a343ad81b5ce2a
AhmedRabee <akotob@surepay.sa>
2026-09-01 13:01:11 +0300

    SBV2-3385 [Merchant Side]UI Misalignment / Incorrect Text Styling for Bank Transfer Status Badges in Latest Bills Widget

--- resources/views/latest_bill_item.blade.php
+++ resources/views/latest_bill_item.blade.php
@@ -1,6 +1,6 @@
 <li class="{{ $loop->last ? '' : 'mb-6' }}">
   <div class="row g-2 align-items-center justify-content-between">
-    <div class="col-12 col-md-8">
+    <div class="col-12 col-md-6">
       <div class="d-flex align-items-center justify-content-start gap-2">
         <div class="badge bg-label-secondary text-body p-2 me-4 rounded">
           <i class="icon-base ti ti-file-invoice icon-md"></i>
@@ -15,12 +15,12 @@
         </div>
       </div><!-- d-flex -->
     </div><!-- col-6 -->
-    <div class="col-6 col-md-2">
+    <div class="col-6 col-md-3">
       <span class="d-flex align-items-center {{app()->getLocale() == 'en' ? 'flex-row-reverse justify-content-end' : 'justify-content-start'}} gap-1 m-0 text-nowrap" style="min-width: 6rem;">
         {{ $bill->fixed_total }} <i class="sar-icon"></i>
       </span>
     </div><!-- col-6 -->
-    <div class="col-6 col-md-2">
+    <div class="col-6 col-md-3">
       <div class="ms-4 flex-shrink-0" style="min-width: 8rem;">@include('bills.status_badge', ['status' => $bill->status, 'id' => $bill->id, 'method' => $bill->method])</div>
     </div><!-- col-6 -->
   </div><!-- row -->
